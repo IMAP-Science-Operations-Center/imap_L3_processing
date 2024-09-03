39c045e4aa12c7a57e756b9211dd2742e1c38e1b
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2024-09-03 14:27:51 -0400

    Harrison 1942 - PMCL/SWYN - Initial use of lookup table for density and temperature

--- imap_processing/swapi/l3a/science/calculate_proton_solar_wind_temperature_and_density.py
+++ imap_processing/swapi/l3a/science/calculate_proton_solar_wind_temperature_and_density.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy
 from matplotlib import pyplot as plt
 from scipy.special import erf
-from uncertainties import correlated_values
+from uncertainties import correlated_values, wrap
 from uncertainties.unumpy import uarray, nominal_values, std_devs
 
 from imap_processing import constants
@@ -15,13 +15,13 @@ from imap_processing.swapi.l3a.science.speed_calculation import find_peak_center
 
 
 def proton_count_rate_model(ev_per_q, density_per_cm3, temperature, bulk_flow_speed_km_per_s):
-    density_per_m3 = density_per_cm3 * CENTIMETERS_PER_METER**3
+    density_per_m3 = density_per_cm3 * CENTIMETERS_PER_METER ** 3
     bulk_flow_speed_meters_per_s = bulk_flow_speed_km_per_s * METERS_PER_KILOMETER
     energy = ev_per_q * constants.PROTON_CHARGE_COULOMBS
     k = BOLTZMANN_CONSTANT_JOULES_PER_KELVIN
     a_eff_cm2 = 3.3e-2 / 1000
     # TODO verify these units
-    a_eff_m2 = a_eff_cm2 / CENTIMETERS_PER_METER**2
+    a_eff_m2 = a_eff_cm2 / CENTIMETERS_PER_METER ** 2
 
     delta_e_over_e = 0.085
     delta_v_over_v = 1 / 2 * delta_e_over_e
@@ -66,10 +66,12 @@ def calculate_proton_solar_wind_temperature_and_density(coincident_count_rates: 
         densities_per_sweep.append(density)
 
     # TODO: check plain or inverse variance average
-    average_temp = np.average(temperatures_per_sweep, weights=1 / std_devs(temperatures_per_sweep)**2)
-    average_density = np.average(densities_per_sweep, weights=1 / std_devs(densities_per_sweep)**2)
+    average_temp = np.average(temperatures_per_sweep, weights=1 / std_devs(temperatures_per_sweep) ** 2)
+    average_density = np.average(densities_per_sweep, weights=1 / std_devs(densities_per_sweep) ** 2)
 
     return average_temp, average_density
+
+
 def calculate_proton_speed_from_one_sweep(coincident_count_rates, energy, proton_peak_indices):
     center_of_mass_index = find_peak_center_of_mass_index(proton_peak_indices, coincident_count_rates)
     energy_at_com = interpolate_energy(center_of_mass_index, energy)
@@ -79,6 +81,37 @@ def calculate_proton_speed_from_one_sweep(coincident_count_rates, energy, proton
 
 def demo(density=5.0, temp=1e5, speed=450):
 
+def build_interpolation_functions_from_lut(lookup_table):
+    solar_wind_speed = np.unique(lookup_table[:, 0])
+    deflection_angle = np.unique(lookup_table[:, 1])
+    clock_angle = np.unique(lookup_table[:, 2])
+    fit_density = np.unique(lookup_table[:, 3])
+    fit_temperature = np.unique(lookup_table[:, 5])
+
+    density_and_temperature_values = lookup_table[:, (4, 6)]
+
+    values_shape = (
+        len(solar_wind_speed), len(deflection_angle), len(clock_angle), len(fit_density), len(fit_temperature), 2)
+    density_and_temperature_grid = density_and_temperature_values.reshape(values_shape)
+
+    grid = (solar_wind_speed, deflection_angle, clock_angle, fit_density, fit_temperature)
+
+    density = wrap(
+        lambda a, b, c, d, e: scipy.interpolate.interpn(grid, density_and_temperature_grid[..., 0], [a, b, c, d, e])[0])
+    temperature = wrap(
+        lambda a, b, c, d, e: scipy.interpolate.interpn(grid, density_and_temperature_grid[..., 1], [a, b, c, d, e])[0])
+
+    return np.vectorize(temperature), np.vectorize(density)
+
+
+def lookup_table_temperature_density(lookup_table, speeds, deflection_angles, clock_angles, densities, temperatures):
+    find_temperature, find_density = build_interpolation_functions_from_lut(lookup_table)
+    return \
+        find_temperature(speeds, deflection_angles, clock_angles, densities, temperatures), \
+            find_density(speeds, deflection_angles, clock_angles, densities, temperatures)
+
+
+def demo(density=5.0, temp=1e5, speed=450):
     voltage = np.geomspace(100, 19000, 62)
 
     plt.loglog(voltage, proton_count_rate_model(voltage, density, temp, speed))
--- imap_processing/tests/swapi/l3a/science/test_calculate_proton_solar_wind_temperature_and_density.py
+++ imap_processing/tests/swapi/l3a/science/test_calculate_proton_solar_wind_temperature_and_density.py
@@ -1,14 +1,16 @@
+import itertools
 from pathlib import Path
 from unittest import TestCase
 
 import numpy as np
 from spacepy.pycdf import CDF
-from uncertainties.unumpy import uarray
+from uncertainties import ufloat
+from uncertainties.unumpy import uarray, nominal_values, std_devs
 
 import imap_processing
 from imap_processing.swapi.l3a.science.calculate_proton_solar_wind_temperature_and_density import \
     calculate_proton_solar_wind_temperature_and_density, proton_count_rate_model, \
-    calculate_proton_solar_wind_temperature_and_density_for_one_sweep
+    calculate_proton_solar_wind_temperature_and_density_for_one_sweep, lookup_table_temperature_density
 from imap_processing.tests.swapi.l3a.science.test_calculate_alpha_solar_wind_speed import synthesize_uncertainties
 
 
@@ -50,6 +52,48 @@ class TestCalculateProtonSolarWindTemperatureAndDensity(TestCase):
         self.assertAlmostEqual(4.674, density.nominal_value, 3)
         self.assertAlmostEqual(5.00e-3, density.std_dev, 5)
 
+    def test_use_density_temperature_lookup_table(self):
+        speed_values = [250, 1000]
+        deflection_angle_values = [0, 6]
+        clock_angle_values = [0, 360]
+        density_values = [1, 10]
+        temperature_values = [1000, 100000]
+
+        lookup_table = self.generate_lookup_table(speed_values, deflection_angle_values, clock_angle_values,
+                                                  density_values, temperature_values)
+        temperature, density = lookup_table_temperature_density(
+            lookup_table, ufloat(450, 2), ufloat(3, 0.1), ufloat(1, 1), ufloat(4, 0.1), ufloat(50000, 10000))
+
+        self.assertAlmostEqual(50000 * 0.97561, nominal_values(temperature), 3)
+        self.assertAlmostEqual(10000 * 0.97561, std_devs(temperature), 3)
+
+        self.assertAlmostEqual(4 * 1.021, nominal_values(density))
+        self.assertAlmostEqual(0.1 * 1.021, std_devs(density))
+
+    def test_use_density_temperature_lookup_table_for_multiple_values(self):
+        speed_values = [250, 1000]
+        deflection_angle_values = [0, 6]
+        clock_angle_values = [0, 360]
+        density_values = [1, 10]
+        temperature_values = [1000, 100000]
+        lookup_table = self.generate_lookup_table(speed_values, deflection_angle_values, clock_angle_values,
+                                                  density_values, temperature_values)
+
+        speeds = uarray([450, 550], [2, 13])
+        deflection_angles = uarray([3, 4], [0.1, 0.2])
+        clock_angles = uarray([265, 270], [5, 6])
+        densities = uarray([4, 4.3], [0.1, 0.2])
+        temperatures = uarray([50000, 60000], [10000, 8000])
+
+        temperatures, densities = lookup_table_temperature_density(
+            lookup_table, speeds, deflection_angles, clock_angles, densities, temperatures)
+
+        np.testing.assert_allclose(nominal_values(temperatures), [50000 * 0.97561, 60000 * 0.97561])
+        np.testing.assert_allclose(std_devs(temperatures), [10000 * 0.97561, 8000 * 0.97561])
+
+        np.testing.assert_allclose(nominal_values(densities), [4 * 1.021, 4.3 * 1.021])
+        np.testing.assert_allclose(std_devs(densities), [0.1 * 1.021, 0.2 * 1.021])
+
     def test_can_recover_density_and_temperature_from_model_data(self):
         test_cases = [
             (5, 100e3, 450),
@@ -71,3 +115,15 @@ class TestCalculateProtonSolarWindTemperatureAndDensity(TestCase):
                     count_rates_with_uncertainties, energy)
                 self.assertAlmostEqual(density, fit_density.nominal_value, 6)
                 self.assertAlmostEqual(temperature, fit_temperature.nominal_value, 0)
+
+    def generate_lookup_table(self, speed_values, deflection_angle_values, clock_angle_values, density_values,
+                              temperature_values):
+
+        coords = (speed_values, deflection_angle_values, clock_angle_values, density_values, temperature_values)
+
+        lut_rows = []
+        for (speed, deflection, clock_angle, density, temperature) in itertools.product(*coords):
+            output_density = 1.021 * density
+            output_temperature = 0.97561 * temperature
+            lut_rows.append([speed, deflection, clock_angle, density, output_density, temperature, output_temperature])
+        return np.array(lut_rows)
