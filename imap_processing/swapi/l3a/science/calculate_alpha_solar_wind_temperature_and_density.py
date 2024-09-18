8a8cd732a26f807d7b4c913d6a244b08c1fd2807
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2024-09-18 16:43:12 -0400

    Harrison 1943 - AFAH/MDEB -  Calculate Alpha sw temperature & density

--- imap_processing/swapi/l3a/science/calculate_alpha_solar_wind_temperature_and_density.py
+++ imap_processing/swapi/l3a/science/calculate_alpha_solar_wind_temperature_and_density.py
@@ -3,6 +3,53 @@ from pathlib import Path
 import numpy as np
 import scipy
 import uncertainties
+from numpy import ndarray
+from scipy.special import erf
+from uncertainties import correlated_values, ufloat
+from uncertainties.unumpy import uarray, nominal_values, std_devs
+
+from imap_processing.constants import BOLTZMANN_CONSTANT_JOULES_PER_KELVIN, METERS_PER_KILOMETER, \
+    CENTIMETERS_PER_METER, ALPHA_PARTICLE_CHARGE_COULOMBS, ALPHA_PARTICLE_MASS_KG
+
+
+def alpha_count_rate_model(ev_per_q, density_per_cm3, temperature, bulk_flow_speed_km_per_s):
+    density_per_m3 = density_per_cm3 * CENTIMETERS_PER_METER ** 3
+    bulk_flow_speed_meters_per_s = bulk_flow_speed_km_per_s * METERS_PER_KILOMETER
+    energy = ev_per_q * ALPHA_PARTICLE_CHARGE_COULOMBS
+    k = BOLTZMANN_CONSTANT_JOULES_PER_KELVIN
+    a_eff_cm2 = 3.3e-2 / 1000
+    a_eff_m2 = a_eff_cm2 / CENTIMETERS_PER_METER ** 2
+
+    delta_e_over_e = 0.085
+    delta_v_over_v = 1 / 2 * delta_e_over_e
+    delta_phi_degrees = 30
+
+    m = ALPHA_PARTICLE_MASS_KG
+    v_th = np.sqrt(2 * k * temperature / m)
+    beta = 1 / v_th ** 2
+    v_e = np.sqrt(2 * energy / m)
+    result = (density_per_m3 * a_eff_m2 * (beta / np.pi) ** (3 / 2) * np.exp(
+        -beta * (v_e ** 2 + bulk_flow_speed_meters_per_s ** 2 - 2 * v_e * bulk_flow_speed_meters_per_s))
+              * np.sqrt(np.pi / (beta * bulk_flow_speed_meters_per_s * v_e))
+              * erf(np.sqrt(beta * bulk_flow_speed_meters_per_s * v_e) * np.radians(delta_phi_degrees / 2))
+              * v_e ** 4 * delta_v_over_v * np.arcsin(v_th / v_e))
+
+    return result
+
+
+def calculate_alpha_solar_wind_temperature_and_density_for_combined_sweeps(peak_coin_rates: uarray,
+                                                                           peak_energies: ndarray,
+                                                                           alpha_sw_speed: ufloat):
+    initial_parameter_guess = [0.15, 3.6e5, nominal_values(alpha_sw_speed)]
+    values, covariance = scipy.optimize.curve_fit(alpha_count_rate_model,
+                                                  peak_energies,
+                                                  nominal_values(peak_coin_rates),
+                                                  sigma=std_devs(peak_coin_rates),
+                                                  absolute_sigma=True,
+                                                  bounds=[[0, 0, 0], [np.inf, np.inf, np.inf]],
+                                                  p0=initial_parameter_guess)
+    density, temperature, speed = correlated_values(values, covariance)
+    return temperature, density
 
 
 class AlphaTemperatureDensityCalibrationTable:
--- tests/swapi/l3a/science/test_calculate_alpha_solar_wind_temperature_and_density.py
+++ tests/swapi/l3a/science/test_calculate_alpha_solar_wind_temperature_and_density.py
@@ -6,7 +6,7 @@ from uncertainties import ufloat
 
 import imap_processing
 from imap_processing.swapi.l3a.science.calculate_alpha_solar_wind_temperature_and_density import \
-    AlphaTemperatureDensityCalibrationTable
+    AlphaTemperatureDensityCalibrationTable, calculate_alpha_solar_wind_temperature_and_density_for_combined_sweeps
 
 
 class TestCalculateAlphaSolarWindTemperatureAndDensity(TestCase):
@@ -16,7 +16,8 @@ class TestCalculateAlphaSolarWindTemperatureAndDensity(TestCase):
 
         calibration_table = AlphaTemperatureDensityCalibrationTable.from_file(file_path)
 
-        self.assertEqual((2, 11, 15), (len(calibration_table.grid[0]), len(calibration_table.grid[1]), len(calibration_table.grid[2])))
+        self.assertEqual((2, 11, 15), (
+            len(calibration_table.grid[0]), len(calibration_table.grid[1]), len(calibration_table.grid[2])))
         self.assertEqual((2, 11, 15), calibration_table.density_grid.shape)
         self.assertEqual((2, 11, 15), calibration_table.temperature_grid.shape)
 
@@ -26,3 +27,22 @@ class TestCalculateAlphaSolarWindTemperatureAndDensity(TestCase):
 
         self.assertEqual(2.5525, calibration_table.lookup_density(sw_speed, density, temperature).nominal_value)
         self.assertEqual(5.8537e+04, calibration_table.lookup_temperature(sw_speed, density, temperature).nominal_value)
+
+    def test_calculate_alpha_solar_wind_temperature_and_density_for_combined_sweeps(self):
+        peak_energies = np.array([2944, 2705, 2485, 2281, 2094])
+        speed = ufloat(496.490, 2.811)
+        peak_coincidence_rates = np.array([
+            ufloat(15.2, 4.2708313008125245),
+            ufloat(25.8, 5.5641710972974225),
+            ufloat(26.0, 5.585696017507577),
+            ufloat(15.4, 4.298837052040936),
+            ufloat(5.8, 2.638181191654584),
+        ])
+
+        actual_temperature, actual_density = calculate_alpha_solar_wind_temperature_and_density_for_combined_sweeps(
+            peak_coincidence_rates, peak_energies, speed)
+
+        self.assertAlmostEqual(411405.2171052396, actual_temperature.nominal_value)
+        self.assertEqual(130246.773340626, actual_temperature.std_dev)
+        self.assertEqual(0.10300409775543028, actual_density.nominal_value)
+        self.assertEqual(0.013108888715912525, actual_density.std_dev)
