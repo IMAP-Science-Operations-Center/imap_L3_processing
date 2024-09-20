78f21a9a1a6b035316ad2bb8356ce461e04205b9
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2024-09-20 10:49:56 -0400

    Harrison 1692 - SWYN/TLAY - Rename fake L2 data for testing our container within AWS

--- run_local_docker.sh
+++ run_local_docker.sh
@@ -1,3 +1,3 @@
-docker run --rm -it --volume="$(pwd)/spice_kernels:/mnt/spice" $(docker build -q .)  --instrument swapi --level l3a \
---start_date 20110101 --end_date 20110101 --version v002 --dependency \
+docker run --rm -it --volume="$(pwd)/spice_kernels:/mnt/spice" $(docker build -q .)  --instrument swapi --data-level l3a \
+--start-date 20110101 --version v002 --dependency \
 """[{'instrument':'swapi', 'data_level':'l2', 'descriptor':'sci', 'version':'v001'}]"""
