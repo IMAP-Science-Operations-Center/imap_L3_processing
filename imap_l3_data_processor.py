c4db6787033c21bcaf2d6178e38510860beb3044
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2024-09-26 16:34:14 -0400

    Harrison 2041 - AFAH/SWYN - Adding test to imap_l3_data_processor

--- imap_l3_data_processor.py
+++ imap_l3_data_processor.py
@@ -1,39 +1,46 @@
+import argparse
+import json
 from datetime import datetime
 
 from imap_processing.models import UpstreamDataDependency, InputMetadata
 from imap_processing.swapi.processor import SwapiProcessor
-import argparse
-import json
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--instrument")
-parser.add_argument("--data-level")
-parser.add_argument("--descriptor")
-parser.add_argument("--start-date")
-parser.add_argument("--end-date", required=False)
-parser.add_argument("--version")
-parser.add_argument("--dependency")
-parser.add_argument(
-    "--upload-to-sdc",
-    action="store_true",
-    required=False,
-    help="Upload completed output files to the IMAP SDC.",
-)
 
-args = parser.parse_args()
-dependencies_list = json.loads(args.dependency.replace("'", '"'))
+def imap_l3_processor():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--instrument")
+    parser.add_argument("--data-level")
+    parser.add_argument("--descriptor")
+    parser.add_argument("--start-date")
+    parser.add_argument("--end-date", required=False)
+    parser.add_argument("--version")
+    parser.add_argument("--dependency")
+    parser.add_argument(
+        "--upload-to-sdc",
+        action="store_true",
+        required=False,
+        help="Upload completed output files to the IMAP SDC.",
+    )
 
-if args.instrument == 'swapi':
-    if args.data_level == 'l3a' or args.data_level == 'l3b':
+    args = parser.parse_args()
+    dependencies_list = json.loads(args.dependency.replace("'", '"'))
+
+    if args.instrument == 'swapi' and (args.data_level == 'l3a' or args.data_level == 'l3b'):
         dependencies = [UpstreamDataDependency(d['instrument'], d['data_level'],
                                                None, None,
                                                d['version'], d['descriptor']) for d in dependencies_list]
         input_dependency = InputMetadata(args.instrument,
                                          args.data_level,
                                          datetime.strptime(args.start_date, '%Y%m%d'),
-                                         None,
+                                         datetime.strptime(args.end_date,
+                                                           '%Y%m%d') if args.end_date is not None else None,
                                          args.version)
         processor = SwapiProcessor(dependencies, input_dependency)
         processor.process()
-else:
-    raise NotImplemented(f'Level {args.level} data processing has not yet been implemented for {args.instrument}')
+    else:
+        raise NotImplementedError(
+            f'Level {args.data_level} data processing has not yet been implemented for {args.instrument}')
+
+
+if __name__ == '__main__':
+    imap_l3_processor()
---
+++ tests/test_imap_l3_data_processor.py
@@ -0,0 +1,104 @@
+from datetime import datetime
+from unittest import TestCase
+from unittest.mock import patch, call
+
+from imap_l3_data_processor import imap_l3_processor
+from imap_processing.models import InputMetadata, UpstreamDataDependency
+
+
+class TestImapL3DataProcessor(TestCase):
+    @patch('imap_l3_data_processor.SwapiProcessor')
+    @patch('imap_l3_data_processor.argparse')
+    def test_runs_swapi_processor_when_instrument_argument_is_swapi(self, mock_argparse, mock_swapi_processor_class):
+        cases = [("20170630", datetime(2017, 6, 30)), (None, None)]
+
+        instrument_argument = "swapi"
+        data_level_argument = "l3a"
+        start_date_argument = "20160630"
+        version_argument = "v092"
+        dependencies_argument = "[{'instrument':'not_swapi', 'data_level':'l1000', 'descriptor':'science', 'version':'v112'}]"
+
+        mock_argument_parser = mock_argparse.ArgumentParser.return_value
+
+        mock_argument_parser.parse_args.return_value.instrument = instrument_argument
+        mock_argument_parser.parse_args.return_value.data_level = data_level_argument
+        mock_argument_parser.parse_args.return_value.dependency = dependencies_argument
+        mock_argument_parser.parse_args.return_value.start_date = start_date_argument
+        mock_argument_parser.parse_args.return_value.version = version_argument
+
+        mock_swapi_processor = mock_swapi_processor_class.return_value
+
+        for input_end_date, expected_end_date in cases:
+            with self.subTest(input_end_date):
+                mock_argument_parser.parse_args.return_value.end_date = input_end_date
+
+                imap_l3_processor()
+
+                parser = mock_argparse.ArgumentParser()
+                parser.add_argument.assert_has_calls([
+                    call("--instrument"),
+                    call("--data-level"),
+                    call("--descriptor"),
+                    call("--start-date"),
+                    call("--end-date", required=False),
+                    call("--version"),
+                    call("--dependency"),
+                    call("--upload-to-sdc", action="store_true", required=False,
+                         help="Upload completed output files to the IMAP SDC.")
+                ])
+
+                expected_input_dependencies = [UpstreamDataDependency("not_swapi",
+                                                                      "l1000",
+                                                                      None,
+                                                                      None,
+                                                                      "v112",
+                                                                      "science")]
+
+                expected_input_metadata = InputMetadata("swapi", "l3a", datetime(year=2016, month=6, day=30),
+                                                        expected_end_date, "v092")
+
+                mock_swapi_processor_class.assert_called_with(expected_input_dependencies, expected_input_metadata)
+
+                mock_swapi_processor.process.assert_called()
+
+    @patch('imap_l3_data_processor.argparse')
+    def test_throws_exception_for_unimplemented_instrument(self, mock_argparse):
+        instrument_argument = "new_instrument"
+        data_level_argument = "l3a"
+        start_date_argument = "20160630"
+        version_argument = "v092"
+        dependencies_argument = "[{'instrument':'not_swapi', 'data_level':'l1000', 'descriptor':'science', 'version':'v112'}]"
+
+        mock_argument_parser = mock_argparse.ArgumentParser.return_value
+
+        mock_argument_parser.parse_args.return_value.instrument = instrument_argument
+        mock_argument_parser.parse_args.return_value.data_level = data_level_argument
+        mock_argument_parser.parse_args.return_value.dependency = dependencies_argument
+        mock_argument_parser.parse_args.return_value.start_date = start_date_argument
+        mock_argument_parser.parse_args.return_value.version = version_argument
+
+        with self.assertRaises(NotImplementedError) as exception_manager:
+            imap_l3_processor()
+        self.assertEqual(str(exception_manager.exception),
+                         "Level l3a data processing has not yet been implemented for new_instrument")
+
+    @patch('imap_l3_data_processor.argparse')
+    def test_throws_exception_when_attempting_to_process_non_l3_data_levels(self, mock_argparse):
+        instrument_argument = "swapi"
+        data_level_argument = "l4"
+        start_date_argument = "20160630"
+        version_argument = "v092"
+        dependencies_argument = "[{'instrument':'not_swapi', 'data_level':'l1000', 'descriptor':'science', 'version':'v112'}]"
+
+        mock_argument_parser = mock_argparse.ArgumentParser.return_value
+
+        mock_argument_parser.parse_args.return_value.instrument = instrument_argument
+        mock_argument_parser.parse_args.return_value.data_level = data_level_argument
+        mock_argument_parser.parse_args.return_value.dependency = dependencies_argument
+        mock_argument_parser.parse_args.return_value.start_date = start_date_argument
+        mock_argument_parser.parse_args.return_value.version = version_argument
+
+        with self.assertRaises(NotImplementedError) as exception_manager:
+            imap_l3_processor()
+        self.assertEqual(str(exception_manager.exception),
+                         "Level l4 data processing has not yet been implemented for swapi")
