ba0205bd6edf949311c7529d6d86e3fb7b1f53af
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2024-10-07 17:03:31 -0400

    Harrison 2052 - TLAY/PMCL - Add function to calculate model count rate denominator.

--- imap_processing/swapi/l3a/science/calculate_pickup_ion.py
+++ imap_processing/swapi/l3a/science/calculate_pickup_ion.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
 
 import numpy as np
+import scipy
 
-from imap_processing.constants import HYDROGEN_BULK_VELOCITY_IN_KM_PER_SECOND, PROTON_MASS_KG, PROTON_CHARGE_COULOMBS
+from imap_processing.constants import HYDROGEN_BULK_VELOCITY_IN_KM_PER_SECOND, PROTON_MASS_KG, PROTON_CHARGE_COULOMBS, \
+    PUI_PARTICLE_MASS_KG, PUI_PARTICLE_CHARGE_COULOMBS
 from imap_processing.spice_wrapper import fake_spice_context
+from imap_processing.swapi.l3a.science.calculate_proton_solar_wind_speed import calculate_sw_speed
 from imap_processing.swapi.l3b.science.geometric_factor_calibration_table import GeometricFactorCalibrationTable
+from imap_processing.swapi.l3b.science.instrument_response_lookup_table import InstrumentResponseLookupTable
 
 
 class DensityOfNeutralHeliumLookupTable:
@@ -32,8 +36,13 @@ class PickupIonModelFit:
         #
         # return first_term * second_term * w_term * neutral_helium_density * step_function
 
-    def fit_function(self):
-        pass
+def _model_count_rate_denominator(response_lookup_table: InstrumentResponseLookupTable) -> float:
+    colatitude_radians = np.deg2rad(90 - response_lookup_table.elevation)
+
+    rows = response_lookup_table.d_energy * np.cos(
+        colatitude_radians) * response_lookup_table.d_elevation * response_lookup_table.d_azimuth
+
+    return rows.sum()
 
 
 def calculate_pui_energy_cutoff(epoch, bulk_flow_speed):
