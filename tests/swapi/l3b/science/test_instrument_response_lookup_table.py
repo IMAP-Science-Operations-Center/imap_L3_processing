e29e681b01601989ad93f11ecd7870172b127483
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2024-10-18 15:21:54 -0400

    Harrison 2055 - ASEG/PMCL - Add complete set of instrument response lookup tables for testing

--- tests/swapi/l3b/science/test_instrument_response_lookup_table.py
+++ tests/swapi/l3b/science/test_instrument_response_lookup_table.py
@@ -6,7 +6,7 @@ from tests.test_helpers import get_test_data_path
 
 class TestInstrumentResponseLookupTable(unittest.TestCase):
     def test_get_table_for_energy_bin(self):
-        lut_directory = get_test_data_path('../swapi/test_data/swapi_response_simion_v1')
+        lut_directory = get_test_data_path('../swapi/test_data/truncated_swapi_response_simion_v1')
         lut = InstrumentResponseLookupTableCollection(lut_directory)
         energy_bin_index = 2
         result = lut.get_table_for_energy_bin(energy_bin_index)
