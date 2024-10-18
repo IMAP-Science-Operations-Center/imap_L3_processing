Harrison 2055 - ASEG/PMCL - WIP: corrected tests to fix various failures and to account for switching to use sin over cos, scaling the velocity to be represented in meters, accommodating vectorized integral

imap_processing/swapi/l3a/science/calculate_pickup_ion.py
@@ -24,7 +24,7 @@ def calculate_pickup_ion_values(instrument_response_lookup_table, geometric_fact
                                 energy: list[float],
                                 count_rates: uarray, epoch: datetime,
                                 background_count_rate_cutoff: float) -> FittingParameters:
-    initial_guess = np.array([2.0, 1e-5, 500.0, 0.1])
+    initial_guess = np.array([1.4, 1, 1, 0.1])
     energy_labels = range(62, 0, -1)
     # calculate sw_velocity_in_imap_frame
     sw_velocity_in_imap_frame = np.array([0, 0, -500])
@@ -34,8 +34,6 @@ def calculate_pickup_ion_values(instrument_response_lookup_table, geometric_fact
                                                                                                  energy_cutoff,
                                                                                                  background_count_rate_cutoff)
 
-    # observed_count_rates: np.ndarray,
-    # indices_and_energy_centers: list[tuple[int, float]], calculator: ModelCountRateCalculator
     model_count_rate_calculator = ModelCountRateCalculator(instrument_response_lookup_table,
                                                            geometric_factor_calibration_table)
     indices = list(zip(extracted_energy_labels, extracted_energies))
@@ -82,7 +80,7 @@ class ForwardModel:
         pui_vector_gse_frame = convert_velocity_relative_to_imap(pui_vector_instrument_frame, ephemeris_time,
                                                                  "IMAP_SWAPI", "GSE")
         pui_vector_solar_wind_frame = pui_vector_gse_frame - self.solar_wind_vector_gse_frame
-        magnitude = np.linalg.norm(pui_vector_solar_wind_frame, axis=1)
+        magnitude = np.linalg.norm(pui_vector_solar_wind_frame, axis=-1)
         w = magnitude / self.fitting_params.cutoff_speed
         imap_position_eclip2000_frame_state = spiceypy.spkezr("IMAP", ephemeris_time, "ECLIPJ2000", "NONE", "SUN")[0:3]
         distance, longitude, latitude = spiceypy.reclat(imap_position_eclip2000_frame_state)
@@ -147,7 +145,7 @@ class ModelCountRateCalculator:
 def calc_chi_squared(fit_params_array: np.ndarray, observed_count_rates: np.ndarray,
                      indices_and_energy_centers: list[tuple[int, float]], calculator: ModelCountRateCalculator):
     cooling_index, ionization_rate, cutoff_speed, background_count_rate = fit_params_array
-    fit_params = FittingParameters(cooling_index, ionization_rate, cutoff_speed, background_count_rate)
+    fit_params = FittingParameters(cooling_index, 1e-10 * ionization_rate, 900 * cutoff_speed, background_count_rate)
     modeled_rates = calculator.model_count_rate(indices_and_energy_centers, fit_params)
 
     energies = [energy for index, energy in indices_and_energy_centers]

tests/swapi/l3a/science/test_calculate_pickup_ion.py
@@ -50,7 +50,7 @@ class TestCalculatePickupIon(unittest.TestCase):
 
         velocity_cutoff_vector = np.array([-3, 0, 4])
         velocity_cutoff_norm = 5
-        self.assertAlmostEqual(0.5 * (PROTON_MASS_KG / PROTON_CHARGE_COULOMBS) * (2 * velocity_cutoff_norm) ** 2,
+        self.assertAlmostEqual(0.5 * (PROTON_MASS_KG / PROTON_CHARGE_COULOMBS) * (2 * velocity_cutoff_norm * 1000) ** 2,
                                energy_cutoff)
 
     def test_extract_pui_energy_bins(self):
@@ -78,8 +78,8 @@ class TestCalculatePickupIon(unittest.TestCase):
                                                      )
         result = _model_count_rate_denominator(lookup_table)
 
-        expected = 0.97411 * np.cos(np.deg2rad(90 - 2)) * 1.0 * 1.0 + \
-                   0.99269 * np.cos(np.deg2rad(90 - 1.0)) * 1.0 * 1.0
+        expected = 0.97411 * np.sin(np.deg2rad(90 - 2)) * 1.0 * 1.0 + \
+                   0.99269 * np.sin(np.deg2rad(90 - 1.0)) * 1.0 * 1.0
         self.assertEqual(expected, result)
 
     @patch("imap_processing.swapi.l3a.science.calculate_pickup_ion.spiceypy")
@@ -227,6 +227,8 @@ class TestCalculatePickupIon(unittest.TestCase):
         model_count_rate_calculator = ModelCountRateCalculator(mock_instrument_response_lut_collection, geometric_table)
 
         fitting_params = FittingParameters(0.23, 0.57, 0.91, 1.23)
+        mock_forward_model.return_value.fitting_params = fitting_params
+
         result = model_count_rate_calculator.model_count_rate([
             (energy_bin_index, energy_bin_center),
             (2, 8000),
@@ -234,19 +236,17 @@ class TestCalculatePickupIon(unittest.TestCase):
         ], fitting_params)
 
         expected_geo_factor = 6.4e-13 / 2
-        expected_denominator = (0.97411 * np.cos(np.deg2rad(90 - 2.0)) * 1.0 * 1.0) + \
-                               (0.99269 * np.cos(np.deg2rad(90 - 1.0)) * 1.0 * 1.0)
-        expected_result = expected_geo_factor * expected_integral_results / expected_denominator
+        expected_denominator = (0.97411 * np.sin(np.deg2rad(90 - 2.0)) * 1.0 * 1.0) + \
+                               (0.99269 * np.sin(np.deg2rad(90 - 1.0)) * 1.0 * 1.0)
+        expected_result = expected_geo_factor * expected_integral_results / expected_denominator + fitting_params.background_count_rate
         np.testing.assert_array_equal(expected_result, result)
 
-        actual_fitting_params, actual_epoch, actual_sw_gse_vector, actual_sw_hci_vector = mock_forward_model.call_args.args
+        actual_fitting_params, actual_epoch, actual_sw_gse_vector, actual_sw_hci_vector_norm = mock_forward_model.call_args.args
         self.assertIs(fitting_params, actual_fitting_params)
         self.assertEqual(datetime(2010, 1, 1), actual_epoch)
-
+        expected_sw_hci_vector_norm = 455.521679
         np.testing.assert_array_equal(expected_sw_gse_vector, actual_sw_gse_vector)
-        np.testing.assert_array_equal(expected_sw_hci_vector, actual_sw_hci_vector)
-        print(mock_convert_velocity_to_reference_frame.call_args_list)
-        print(sw_instrument_frame_vector)
+        self.assertAlmostEqual(expected_sw_hci_vector_norm, actual_sw_hci_vector_norm)
         mock_calculate_velocity_vector.assert_called_with(*l3a_calculation())
         mock_convert_velocity_to_reference_frame.assert_has_calls([
             call(sw_instrument_frame_vector, ephemeris_time_for_epoch, "IMAP_RTN", "GSE"),
@@ -276,12 +276,12 @@ class TestCalculatePickupIon(unittest.TestCase):
 
         expected_row_1_colatitude = 90 - 2.0
         expected_row_1_forward_model_f = 33.33
-        expected_row_1 = 0.0160000000 * expected_row_1_forward_model_f * expected_speed_row_1 ** 4 * 0.97411 * np.cos(
+        expected_row_1 = 0.0160000000 * expected_row_1_forward_model_f * expected_speed_row_1 ** 4 * 0.97411 * np.sin(
             np.deg2rad(expected_row_1_colatitude)) * 1.0 * 3.0
 
         expected_row_2_colatitude = 90 - 1.1
         expected_row_2_forward_model_f = 35.55
-        expected_row_2 = 0.0230000000 * expected_row_2_forward_model_f * expected_speed_row_2 ** 4 * 0.99269 * np.cos(
+        expected_row_2 = 0.0230000000 * expected_row_2_forward_model_f * expected_speed_row_2 ** 4 * 0.99269 * np.sin(
             np.deg2rad(expected_row_2_colatitude)) * 1.5 * 2.0
 
         np.testing.assert_array_equal(result, expected_row_1 + expected_row_2)