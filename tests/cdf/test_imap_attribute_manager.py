be5fc739d2bd3d9f4814fd5fcfb4f148b4130030
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2024-11-20 16:02:07 -0500

    Harrison - PMCL/SWYN/JNIE - Pull down the new sammi-cdf metadata library, adjust usage to work

--- tests/cdf/test_imap_attribute_manager.py
+++ tests/cdf/test_imap_attribute_manager.py
@@ -8,110 +8,111 @@ from imap_processing.cdf.imap_attribute_manager import ImapAttributeManager
 
 
 class TestImapCdfManager(TestCase):
+    def setUp(self):
+        self.config_folder_path = Path(imap_processing.__file__).parent.resolve() / 'cdf/config'
+        self.base_manager = CdfAttributeManager(
+            variable_schema_layers=[self.config_folder_path / 'imap_l3_variable_cdf_attrs_schema.yaml'],
+            use_defaults=True)
+        self.base_manager.load_global_attributes(self.config_folder_path / 'imap_default_global_cdf_attrs.yaml')
+
     def test_constructor(self):
         manager = ImapAttributeManager()
         self.assertIsInstance(manager, CdfAttributeManager)
 
-        base_manager = CdfAttributeManager(Path(f'{Path(imap_processing.__file__).parent.resolve()}/cdf/config'))
-
-        base_manager.load_global_attributes('imap_default_global_cdf_attrs.yaml')
-        self.assertEqual(base_manager.get_global_attributes(), manager.get_global_attributes())
+        self.assertEqual(self.base_manager.get_global_attributes(), manager.get_global_attributes())
 
     def test_load_instrument_and_variable_attributes_with_level(self):
         manager = ImapAttributeManager()
         manager.add_instrument_attrs('swapi', 'l3a')
 
-        base_manager = CdfAttributeManager(Path(f'{Path(imap_processing.__file__).parent.resolve()}/cdf/config'))
-        base_manager.load_global_attributes('imap_default_global_cdf_attrs.yaml')
-        base_manager.load_global_attributes('imap_swapi_global_cdf_attrs.yaml')
-        base_manager.load_global_attributes('imap_swapi_l3a_global_cdf_attrs.yaml')
-        base_manager.load_variable_attributes('imap_swapi_l3a_variable_attrs.yaml')
-        self.assertEqual(base_manager.get_global_attributes(), manager.get_global_attributes())
-        self.assertEqual(base_manager.get_variable_attributes('epoch'), manager.get_variable_attributes('epoch'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_speed_delta'),
+        self.base_manager.load_global_attributes(self.config_folder_path / 'imap_swapi_global_cdf_attrs.yaml')
+        self.base_manager.load_global_attributes(self.config_folder_path / 'imap_swapi_l3a_global_cdf_attrs.yaml')
+        self.base_manager.load_variable_attributes(self.config_folder_path / 'imap_swapi_l3a_variable_attrs.yaml')
+
+        self.assertEqual(self.base_manager.get_global_attributes(), manager.get_global_attributes())
+        self.assertEqual(self.base_manager.get_variable_attributes('epoch'), manager.get_variable_attributes('epoch'))
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_speed_delta'),
                          manager.get_variable_attributes('proton_sw_speed_delta'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_speed'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_speed'),
                          manager.get_variable_attributes('proton_sw_speed'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_clock_angle'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_clock_angle'),
                          manager.get_variable_attributes('proton_sw_clock_angle'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_clock_angle_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_clock_angle_delta'),
                          manager.get_variable_attributes('proton_sw_clock_angle_delta'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_deflection_angle'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_deflection_angle'),
                          manager.get_variable_attributes('proton_sw_deflection_angle'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_deflection_angle_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_deflection_angle_delta'),
                          manager.get_variable_attributes('proton_sw_deflection_angle_delta'))
 
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_speed'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_speed'),
                          manager.get_variable_attributes('alpha_sw_speed'))
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_speed_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_speed_delta'),
                          manager.get_variable_attributes('alpha_sw_speed_delta'))
 
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_density'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_density'),
                          manager.get_variable_attributes('alpha_sw_density'))
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_density_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_density_delta'),
                          manager.get_variable_attributes('alpha_sw_density_delta'))
 
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_temperature'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_temperature'),
                          manager.get_variable_attributes('alpha_sw_temperature'))
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_temperature_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_temperature_delta'),
                          manager.get_variable_attributes('alpha_sw_temperature_delta'))
 
     def test_l3b_metadata_configuration(self):
         manager = ImapAttributeManager()
         manager.add_instrument_attrs('swapi', 'l3b')
 
-        base_manager = CdfAttributeManager(Path(f'{Path(imap_processing.__file__).parent.resolve()}/cdf/config'))
-        base_manager.load_global_attributes('imap_default_global_cdf_attrs.yaml')
-        base_manager.load_global_attributes('imap_swapi_global_cdf_attrs.yaml')
-        base_manager.load_global_attributes('imap_swapi_l3b_global_cdf_attrs.yaml')
-        base_manager.load_variable_attributes('imap_swapi_l3b_variable_attrs.yaml')
-        self.assertEqual(base_manager.get_global_attributes(), manager.get_global_attributes())
-        self.assertEqual(base_manager.get_variable_attributes('epoch'), manager.get_variable_attributes('epoch'))
+        self.base_manager.load_global_attributes(self.config_folder_path / 'imap_swapi_global_cdf_attrs.yaml')
+        self.base_manager.load_global_attributes(self.config_folder_path / 'imap_swapi_l3b_global_cdf_attrs.yaml')
+        self.base_manager.load_variable_attributes(self.config_folder_path / 'imap_swapi_l3b_variable_attrs.yaml')
+        self.assertEqual(self.base_manager.get_global_attributes(), manager.get_global_attributes())
+        self.assertEqual(self.base_manager.get_variable_attributes('epoch'), manager.get_variable_attributes('epoch'))
 
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_velocity'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_velocity'),
                          manager.get_variable_attributes('proton_sw_velocity'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_velocity_delta_minus'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_velocity_delta_minus'),
                          manager.get_variable_attributes('proton_sw_velocity_delta_minus'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_velocity_delta_plus'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_velocity_delta_plus'),
                          manager.get_variable_attributes('proton_sw_velocity_delta_plus'))
 
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_vdf'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_vdf'),
                          manager.get_variable_attributes('proton_sw_vdf'))
-        self.assertEqual(base_manager.get_variable_attributes('proton_sw_vdf_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('proton_sw_vdf_delta'),
                          manager.get_variable_attributes('proton_sw_vdf_delta'))
 
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_velocity'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_velocity'),
                          manager.get_variable_attributes('alpha_sw_velocity'))
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_velocity_delta_minus'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_velocity_delta_minus'),
                          manager.get_variable_attributes('alpha_sw_velocity_delta_minus'))
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_velocity_delta_plus'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_velocity_delta_plus'),
                          manager.get_variable_attributes('alpha_sw_velocity_delta_plus'))
 
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_vdf'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_vdf'),
                          manager.get_variable_attributes('alpha_sw_vdf'))
-        self.assertEqual(base_manager.get_variable_attributes('alpha_sw_vdf_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('alpha_sw_vdf_delta'),
                          manager.get_variable_attributes('alpha_sw_vdf_delta'))
 
-        self.assertEqual(base_manager.get_variable_attributes('pui_sw_velocity'),
+        self.assertEqual(self.base_manager.get_variable_attributes('pui_sw_velocity'),
                          manager.get_variable_attributes('pui_sw_velocity'))
-        self.assertEqual(base_manager.get_variable_attributes('pui_sw_velocity_delta_minus'),
+        self.assertEqual(self.base_manager.get_variable_attributes('pui_sw_velocity_delta_minus'),
                          manager.get_variable_attributes('pui_sw_velocity_delta_minus'))
-        self.assertEqual(base_manager.get_variable_attributes('pui_sw_velocity_delta_plus'),
+        self.assertEqual(self.base_manager.get_variable_attributes('pui_sw_velocity_delta_plus'),
                          manager.get_variable_attributes('pui_sw_velocity_delta_plus'))
 
-        self.assertEqual(base_manager.get_variable_attributes('pui_sw_vdf'),
+        self.assertEqual(self.base_manager.get_variable_attributes('pui_sw_vdf'),
                          manager.get_variable_attributes('pui_sw_vdf'))
-        self.assertEqual(base_manager.get_variable_attributes('pui_sw_vdf_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('pui_sw_vdf_delta'),
                          manager.get_variable_attributes('pui_sw_vdf_delta'))
 
-        self.assertEqual(base_manager.get_variable_attributes('combined_energy'),
+        self.assertEqual(self.base_manager.get_variable_attributes('combined_energy'),
                          manager.get_variable_attributes('combined_energy'))
-        self.assertEqual(base_manager.get_variable_attributes('combined_energy_delta_minus'),
+        self.assertEqual(self.base_manager.get_variable_attributes('combined_energy_delta_minus'),
                          manager.get_variable_attributes('combined_energy_delta_minus'))
-        self.assertEqual(base_manager.get_variable_attributes('combined_energy_delta_plus'),
+        self.assertEqual(self.base_manager.get_variable_attributes('combined_energy_delta_plus'),
                          manager.get_variable_attributes('combined_energy_delta_plus'))
 
-        self.assertEqual(base_manager.get_variable_attributes('combined_differential_flux'),
+        self.assertEqual(self.base_manager.get_variable_attributes('combined_differential_flux'),
                          manager.get_variable_attributes('combined_differential_flux'))
-        self.assertEqual(base_manager.get_variable_attributes('combined_differential_flux_delta'),
+        self.assertEqual(self.base_manager.get_variable_attributes('combined_differential_flux_delta'),
                          manager.get_variable_attributes('combined_differential_flux_delta'))
