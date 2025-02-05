8f2a0f9e6306947580d6fa3e8f9cc8caf1d5178f
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-02-05 16:35:35 -0500

    Harrison 2371 - SWYN/PMCL - remove broken import

--- imap_processing/hit/l3/hit_processor.py
+++ imap_processing/hit/l3/hit_processor.py
@@ -1,11 +1,4 @@
-import imap_data_access
-from spacepy.pycdf import CDF
-from spiceypy import spiceypy
-
-from imap_processing import utils
-from imap_processing.hit.l3.utils import read_l2_hit_data, calculate_unit_vector
 from imap_processing.processor import Processor
-from imap_processing.utils import format_time, read_l1d_mag_data
 
 
 class HitProcessor(Processor):
