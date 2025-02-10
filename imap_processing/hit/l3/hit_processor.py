05b3e7daa0ea97b83a734f3d83b22bf8815b798f
Pleasant <pleasant@menloinnovations.com>
2025-02-10 17:05:04 -0500

    Harrison 2372 - TLAY/SWYN - Wire up the HitProcessor to fetch dependencies, calculate vectors, and rebin the data.

--- imap_processing/hit/l3/hit_processor.py
+++ imap_processing/hit/l3/hit_processor.py
@@ -1,28 +1,37 @@
 import numpy as np
 
+from imap_processing.hit.l3.hit_l3_sectored_dependencies import HITL3SectoredDependencies
 from imap_processing.hit.l3.sectored_products.science.sectored_products_algorithms import get_sector_unit_vectors, \
     get_hit_bin_polar_coordinates, calculate_pitch_angle, calculate_gyrophase, rebin_by_pitch_angle_and_gyrophase, \
-    calculate_unit_vector, calculate_sector_areas
+    calculate_unit_vector, calculate_sector_areas, calculate_average_mag_data
 from imap_processing.processor import Processor
 
 
 class HitProcessor(Processor):
     def process(self):
-        for species in species_list:
-            species_hit_data = hit_data[species]
-            for mag_chunk, hit_l2_chunk in get_ten_minute_chunk(mag_data, species_hit_data):
-                average_mag_vector = np.average(mag_chunk[:, :3], axis=0)
-                mag_unit_vector = calculate_unit_vector(average_mag_vector)
+        number_of_pitch_angle_bins = 8
+        number_of_gyrophase_bins = 15
 
-                dec, dec_delta, inc, inc_delta = get_hit_bin_polar_coordinates()
-                sector_unit_vectors = get_sector_unit_vectors(dec, inc)
-                sector_areas = calculate_sector_areas(dec, dec_delta, inc_delta)
-                particle_unit_vectors = -sector_unit_vectors
-                pitch_angles = calculate_pitch_angle(particle_unit_vectors, mag_unit_vector)
 
-                gyrophases = calculate_gyrophase(particle_unit_vectors, mag_unit_vector)
+        dependencies = HITL3SectoredDependencies.fetch_dependencies(self.dependencies)
+        mag_data = dependencies.mag_l1d_data
+        hit_data = dependencies.data
+        species_list = [hit_data.CNO, hit_data.helium4, hit_data.hydrogen, hit_data.iron, hit_data.NeMgSi]
 
-                number_of_pitch_angle_bins = 8
-                number_of_gyrophase_bins = 15
-                rebinned_data = rebin_by_pitch_angle_and_gyrophase(hit_l2_chunk, pitch_angles, gyrophases, sector_areas,
-                                                                   number_of_pitch_angle_bins, number_of_gyrophase_bins)
+        # sector directions are constant
+        dec, dec_delta, inc, inc_delta = get_hit_bin_polar_coordinates()
+        sector_unit_vectors = get_sector_unit_vectors(dec, inc)
+        particle_unit_vectors = -sector_unit_vectors
+
+        sector_areas = calculate_sector_areas(dec, dec_delta, inc_delta)
+
+        averaged_mag_data = calculate_average_mag_data(mag_data.mag_data, mag_data.epoch, hit_data.epoch, hit_data.epoch_delta)
+        for time_index, average_mag_vector in  enumerate( averaged_mag_data):
+            mag_unit_vector = calculate_unit_vector(average_mag_vector)
+            pitch_angles = calculate_pitch_angle(particle_unit_vectors, mag_unit_vector)
+            gyrophases = calculate_gyrophase(particle_unit_vectors, mag_unit_vector)
+
+            for species_hit_data in species_list:
+                for energy in species_hit_data[time_index]:
+                    rebinned_data = rebin_by_pitch_angle_and_gyrophase(energy, pitch_angles, gyrophases, sector_areas,
+                                                                       number_of_pitch_angle_bins, number_of_gyrophase_bins)
--- tests/hit/l3/test_hit_processor.py
+++ tests/hit/l3/test_hit_processor.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from dataclasses import fields
 from datetime import datetime
 from pathlib import Path
 from unittest import TestCase
@@ -8,6 +9,7 @@ from unittest.mock import sentinel, patch, call, Mock
 import numpy as np
 
 from imap_processing.constants import TEMP_CDF_FOLDER_PATH
+from imap_processing.hit.l3.hit_l3_sectored_dependencies import HITL3SectoredDependencies
 from imap_processing.hit.l3.hit_processor import HitProcessor
 from imap_processing.hit.l3.models import HitL2Data
 from imap_processing.models import UpstreamDataDependency, InputMetadata, MagL1dData
@@ -21,3 +23,91 @@ class TestHitProcessor(TestCase):
             HitProcessor([], Mock()),
             Processor
         )
+
+    @patch('imap_processing.hit.l3.hit_processor.HITL3SectoredDependencies.fetch_dependencies')
+    @patch('imap_processing.hit.l3.hit_processor.calculate_average_mag_data')
+    @patch('imap_processing.hit.l3.hit_processor.calculate_unit_vector')
+    @patch('imap_processing.hit.l3.hit_processor.get_hit_bin_polar_coordinates')
+    @patch('imap_processing.hit.l3.hit_processor.get_sector_unit_vectors')
+    @patch('imap_processing.hit.l3.hit_processor.calculate_sector_areas')
+    @patch('imap_processing.hit.l3.hit_processor.calculate_pitch_angle')
+    @patch('imap_processing.hit.l3.hit_processor.calculate_gyrophase')
+    @patch('imap_processing.hit.l3.hit_processor.rebin_by_pitch_angle_and_gyrophase')
+    def test_can_fetch_dependencies(self, mock_rebin_by_pitch_angle_and_gyrophase, mock_calculate_gyrophase,
+                                    mock_calculate_pitch_angle,
+                                    mock_calculate_sector_areas,
+                                    mock_get_sector_unit_vectors, mock_get_hit_bin_polar_coordinates,
+                                    mock_calculate_unit_vector,
+                                    mock_calculate_average_mag_data,
+                                    mock_fetch_dependencies):
+        epochs = [sentinel.time1, sentinel.time2]
+        averaged_mag_vectors = [sentinel.mag_vector1, sentinel.mag_vector2]
+
+        mock_calculate_average_mag_data.return_value = averaged_mag_vectors
+
+        mock_dependencies = Mock(spec=HITL3SectoredDependencies)
+        mock_mag_data = self.create_dataclass_mock(MagL1dData)
+        mock_dependencies.mag_l1d_data = mock_mag_data
+        mock_hit_data = self.create_dataclass_mock(HitL2Data)
+        mock_hit_data.epoch = epochs
+
+        mock_hit_data.CNO = [[sentinel.time1_energy], [sentinel.time2_energy]]
+        mock_hit_data.helium4 = [[], []]
+        mock_hit_data.hydrogen = [[], []]
+        mock_hit_data.iron = [[], []]
+        mock_hit_data.NeMgSi = [[], []]
+
+        mock_dependencies.data = mock_hit_data
+        mock_fetch_dependencies.return_value = mock_dependencies
+
+        mock_calculate_unit_vector.side_effect = [sentinel.mag_unit_vector1, sentinel.mag_unit_vector2]
+        mock_get_hit_bin_polar_coordinates.return_value = (sentinel.dec, sentinel.dec_delta, sentinel.inc, sentinel.inc_delta)
+
+        sector_unit_vectors = np.array([[1, 0, 0], [0, 1, 0]])
+        mock_get_sector_unit_vectors.return_value = sector_unit_vectors
+        mock_calculate_sector_areas.return_value = sentinel.sector_areas
+
+        mock_calculate_pitch_angle.side_effect = [sentinel.pitch_angle1, sentinel.pitch_angle2]
+        mock_calculate_gyrophase.side_effect = [sentinel.gyrophase1, sentinel.gyrophase2]
+
+
+        processor = HitProcessor(sentinel.upstream_dependency, Mock())
+        processor.process()
+        mock_fetch_dependencies.assert_called_once_with(sentinel.upstream_dependency)
+
+        mock_calculate_average_mag_data.assert_called_once_with(mock_mag_data.mag_data, mock_mag_data.epoch, mock_hit_data.epoch, mock_hit_data.epoch_delta)
+
+        mock_calculate_unit_vector.assert_has_calls([
+            call(sentinel.mag_vector1),
+            call(sentinel.mag_vector2)
+        ])
+        mock_get_sector_unit_vectors.assert_called_once_with(sentinel.dec, sentinel.inc)
+        mock_calculate_sector_areas.assert_called_once_with(sentinel.dec, sentinel.dec_delta, sentinel.inc_delta)
+
+        self.assertEqual(2, mock_calculate_pitch_angle.call_count)
+        np.testing.assert_array_equal(mock_calculate_pitch_angle.call_args_list[0].args[0], -sector_unit_vectors)
+        self.assertEqual(mock_calculate_pitch_angle.call_args_list[0].args[1], sentinel.mag_unit_vector1)
+
+        np.testing.assert_array_equal(mock_calculate_pitch_angle.call_args_list[1].args[0], -sector_unit_vectors)
+        self.assertEqual(mock_calculate_pitch_angle.call_args_list[1].args[1], sentinel.mag_unit_vector2)
+
+        self.assertEqual(2, mock_calculate_gyrophase.call_count)
+        np.testing.assert_array_equal(mock_calculate_gyrophase.call_args_list[0].args[0], -sector_unit_vectors)
+        self.assertEqual(mock_calculate_gyrophase.call_args_list[0].args[1], sentinel.mag_unit_vector1)
+
+        np.testing.assert_array_equal(mock_calculate_gyrophase.call_args_list[1].args[0], -sector_unit_vectors)
+        self.assertEqual(mock_calculate_gyrophase.call_args_list[1].args[1], sentinel.mag_unit_vector2)
+
+
+        number_of_pitch_angle_bins = 8
+        number_of_gyrophase_bins = 15
+
+        mock_rebin_by_pitch_angle_and_gyrophase.assert_has_calls([
+            call(sentinel.time1_energy, sentinel.pitch_angle1, sentinel.gyrophase1, sentinel.sector_areas,
+                 number_of_pitch_angle_bins, number_of_gyrophase_bins),
+            call(sentinel.time2_energy, sentinel.pitch_angle2, sentinel.gyrophase2, sentinel.sector_areas,
+                 number_of_pitch_angle_bins, number_of_gyrophase_bins)
+        ])
+
+    def create_dataclass_mock(self,obj):
+        return Mock(spec=[field.name for field in fields(obj)])
