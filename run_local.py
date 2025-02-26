7c6e6af00b1b9ca8aa311755ce06a31f3ca80226
Pleasant <pleasant@menloinnovations.com>
2025-02-26 15:53:30 -0500

    Harrison 2372 - GLUN/MDEB - Add hit processor call to run_local. Convert CSV to yaml now looks at correct columns.

--- run_local.py
+++ run_local.py
@@ -9,11 +9,14 @@ from imap_processing.glows.descriptors import GLOWS_L2_DESCRIPTOR
 from imap_processing.glows.glows_processor import GlowsProcessor
 from imap_processing.glows.l3a.glows_l3a_dependencies import GlowsL3ADependencies
 from imap_processing.glows.l3a.utils import read_l2_glows_data
+from imap_processing.hit.l3.hit_l3_sectored_dependencies import HITL3SectoredDependencies
+from imap_processing.hit.l3.hit_processor import HitProcessor
 from imap_processing.hit.l3.pha.pha_event_reader import PHAEventReader
 from imap_processing.hit.l3.pha.science.calculate_pha import process_pha_event
 from imap_processing.hit.l3.pha.science.cosine_correction_lookup_table import CosineCorrectionLookupTable
 from imap_processing.hit.l3.pha.science.gain_lookup_table import GainLookupTable
 from imap_processing.hit.l3.pha.science.range_fit_lookup import RangeFitLookup
+from imap_processing.hit.l3.utils import read_l2_hit_data
 from imap_processing.models import InputMetadata, UpstreamDataDependency
 from imap_processing.swapi.l3a.science.calculate_alpha_solar_wind_temperature_and_density import \
     AlphaTemperatureDensityCalibrationTable
@@ -31,7 +34,7 @@ from imap_processing.swapi.l3b.swapi_l3b_dependencies import SwapiL3BDependencie
 from imap_processing.swapi.swapi_processor import SwapiProcessor
 from imap_processing.swe.l3.swe_l3_dependencies import SweL3Dependencies
 from imap_processing.swe.swe_processor import SweProcessor
-from imap_processing.utils import save_data
+from imap_processing.utils import save_data, read_l1d_mag_data
 from tests.test_helpers import get_test_data_path
 
 
@@ -129,6 +132,17 @@ def create_swe_cdf(dependencies: SweL3Dependencies) -> str:
     cdf_path = save_data(output_data)
     return cdf_path
 
+def create_hit_cdf(dependencies: HITL3SectoredDependencies) -> str:
+    input_metadata = InputMetadata(
+        instrument='hit',
+        data_level='l3',
+        start_date=datetime(2025, 10, 23),
+        end_date=datetime(2025, 10, 24),
+        version='v999')
+    processor = HitProcessor(None, input_metadata)
+    output_data = processor._calculate_pitch_angle_products(dependencies)
+    cdf_path = save_data(output_data)
+    return cdf_path
 
 def process_hit_pha():
     bitstream = BitStream(filename=get_test_data_path("hit/pha_events/full_event_record_buffer.bin"))
@@ -189,8 +203,12 @@ if __name__ == "__main__":
         path = create_glows_l3a_cdf(dependencies)
         print(path)
 
-    if "hit" in sys.argv:
-        process_hit_pha()
+    # if "hit" in sys.argv:
+    mag_data = read_l1d_mag_data(get_test_data_path("mag/imap_mag_l1d_mago-normal_20250101_v001.cdf"))
+    hit_data = read_l2_hit_data(get_test_data_path("hit/hit_l2_sectored_sample1_20250101.cdf"))
+    dependencies = HITL3SectoredDependencies(mag_l1d_data=mag_data, data=hit_data)
+    print(create_hit_cdf(dependencies))
+        # process_hit_pha()
 
     if "swe" in sys.argv:
         dependencies = SweL3Dependencies.from_file_paths(
--- scripts/convert_product_definition_to_yaml.py
+++ scripts/convert_product_definition_to_yaml.py
@@ -4,7 +4,7 @@ import sys
 if len(sys.argv) == 2:
     product_definition_file = sys.argv[1]
 else:
-    product_definition_file = "data_product_spec_csvs/hit_l3_sectoredrates_product_definition.csv"
+    product_definition_file = "data_product_spec_csvs/imap_hit_l3_pitch_angle.csv"
 
 with open(product_definition_file) as csvfile:
     csv_reader = csv.reader(csvfile)
@@ -12,9 +12,9 @@ with open(product_definition_file) as csvfile:
     headers = next(csv_reader)
     metadata = list(csv_reader)
     for row in metadata:
-        variable_name = row[1]
-        metadata_strs = ["\t" + f"{cdf_metadata_name}: {cdf_metadata_value}" for cdf_metadata_name, cdf_metadata_value
+        variable_name = row[0]
+        metadata_strs = ["  " + f"{cdf_metadata_name}: {cdf_metadata_value}" for cdf_metadata_name, cdf_metadata_value
                          in
-                         list(zip(headers, row))[4:] if cdf_metadata_value]
+                         list(zip(headers, row))[3:] if cdf_metadata_value]
         variable_section = "\n".join([f"{variable_name}:"] + metadata_strs + [""])
         print(variable_section)
