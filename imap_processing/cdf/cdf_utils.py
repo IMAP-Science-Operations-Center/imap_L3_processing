53496802d8cf48e65accedbc15667783efd5d3c3
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-03-07 17:12:50 -0500

    Harrison 2381 - PMCL/TLAY - Replace NaNs with fill value when writing CDF
    Only do so if the input is a float and the fill value is specified.

--- imap_processing/cdf/cdf_utils.py
+++ imap_processing/cdf/cdf_utils.py
@@ -22,10 +22,14 @@ def write_cdf(file_path: str, data: DataProduct, attribute_manager: ImapAttribut
 
         for data_product in data.to_data_product_variables():
             var_name = data_product.name
-            cdf.new(var_name, data=data_product.value,
+            variable_attributes = attribute_manager.get_variable_attributes(var_name)
+            data_array = np.asarray(data_product.value)
+            if 'FILLVAL' in variable_attributes and np.issubdtype(data_array.dtype, np.floating):
+                data_array = np.where(np.isnan(data_array), variable_attributes["FILLVAL"], data_array)
+            cdf.new(var_name, data_array,
                     recVary=data_product.record_varying,
                     type=data_product.cdf_data_type)
-            for k, v in attribute_manager.get_variable_attributes(var_name).items():
+            for k, v in variable_attributes.items():
                 if k == 'DEPEND_0' and v == '':
                     continue
                 if k == 'FILLVAL' and data_product.cdf_data_type is not None:
