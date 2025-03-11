Harrison 2372 - GLUN/SBAL - Fixed issue with averaging pitch angles in rebinning. Fixed shape of rotation matrix. Reordered species to match convention.

imap_l3_processing/hit/l3/sectored_products/science/sectored_products_algorithms.py
@@ -24,6 +24,7 @@ def get_sector_unit_vectors(declinations_degrees: np.ndarray, inclinations_degre
     sin_dec = np.sin(declinations)
     x = sin_dec * np.cos(inclinations)
     y = sin_dec * np.sin(inclinations)
+
     stacked = np.stack(np.broadcast_arrays(x, y, z), axis=-1)
     return stacked
 
@@ -41,29 +42,50 @@ def rebin_by_pitch_angle_and_gyrophase(flux_data: np.array,
     flux_with_delta_plus = uarray(flux_data, flux_delta_plus)
     flux_with_delta_minus = uarray(flux_data, flux_delta_minus)
 
-    output_shape = (flux_data.shape[0], number_of_pitch_angle_bins, number_of_gyrophase_bins)
-    rebinned_summed_with_delta_plus = uarray(np.zeros(shape=output_shape), 0)
-    rebinned_summed_with_delta_minus = uarray(np.zeros(shape=output_shape), 0)
-    rebinned_count = np.zeros(shape=output_shape)
+    output_shape_pa_and_gyro = (flux_data.shape[0], number_of_pitch_angle_bins, number_of_gyrophase_bins)
+    output_shape_pa_only = (flux_data.shape[0], number_of_pitch_angle_bins)
+    rebinned_summed_by_pa_and_gyro_with_delta_plus = uarray(np.zeros(shape=output_shape_pa_and_gyro), 0)
+    rebinned_summed_by_pa_and_gyro_with_delta_minus = uarray(np.zeros(shape=output_shape_pa_and_gyro), 0)
+
+    rebinned_summed_pa_only_with_delta_plus = uarray(np.zeros(shape=output_shape_pa_only), 0)
+    rebinned_summed_pa_only_with_delta_minus = uarray(np.zeros(shape=output_shape_pa_only), 0)
+
+    rebinned_count_by_pa_and_gyro = np.zeros(shape=output_shape_pa_and_gyro)
+    rebinned_count_pa_only = np.zeros(shape=output_shape_pa_only)
 
     for i, (flux_delta_plus, flux_delta_minus) in enumerate(zip(flux_with_delta_plus, flux_with_delta_minus)):
         for pitch_angle_bin, gyrophase_bin, flux_with_plus, flux_with_minus in zip(np.ravel(pitch_angle_bins),
                                                                                    np.ravel(gyrophase_bins),
                                                                                    np.ravel(flux_delta_plus),
                                                                                    np.ravel(flux_delta_minus)):
-            rebinned_summed_with_delta_plus[i, pitch_angle_bin, gyrophase_bin] += flux_with_plus
-            rebinned_summed_with_delta_minus[i, pitch_angle_bin, gyrophase_bin] += flux_with_minus
+            rebinned_summed_by_pa_and_gyro_with_delta_plus[i, pitch_angle_bin, gyrophase_bin] += flux_with_plus
+            rebinned_summed_by_pa_and_gyro_with_delta_minus[i, pitch_angle_bin, gyrophase_bin] += flux_with_minus
+            rebinned_count_by_pa_and_gyro[i, pitch_angle_bin, gyrophase_bin] += 1
 
-            rebinned_count[i, pitch_angle_bin, gyrophase_bin] += 1
+            rebinned_summed_pa_only_with_delta_plus[i, pitch_angle_bin] += flux_with_plus
+            rebinned_summed_pa_only_with_delta_minus[i, pitch_angle_bin] += flux_with_minus
+            rebinned_count_pa_only[i, pitch_angle_bin] += 1
 
-    averaged_rebinned_fluxes_with_delta_plus = np.divide(rebinned_summed_with_delta_plus, rebinned_count,
-                                                         out=np.full_like(rebinned_summed_with_delta_plus, np.nan),
-                                                         where=rebinned_count != 0)
-    averaged_rebinned_fluxes_with_delta_minus = np.divide(rebinned_summed_with_delta_minus, rebinned_count,
-                                                          out=np.full_like(rebinned_summed_with_delta_minus, np.nan),
-                                                          where=rebinned_count != 0)
-    pa_only_with_delta_plus = np.nanmean(averaged_rebinned_fluxes_with_delta_plus, axis=-1)
-    pa_only_with_delta_minus = np.nanmean(averaged_rebinned_fluxes_with_delta_minus, axis=-1)
+    averaged_rebinned_fluxes_with_delta_plus = np.divide(rebinned_summed_by_pa_and_gyro_with_delta_plus,
+                                                         rebinned_count_by_pa_and_gyro,
+                                                         out=np.full_like(
+                                                             rebinned_summed_by_pa_and_gyro_with_delta_plus, np.nan),
+                                                         where=rebinned_count_by_pa_and_gyro != 0)
+    averaged_rebinned_fluxes_with_delta_minus = np.divide(rebinned_summed_by_pa_and_gyro_with_delta_minus,
+                                                          rebinned_count_by_pa_and_gyro,
+                                                          out=np.full_like(
+                                                              rebinned_summed_by_pa_and_gyro_with_delta_minus, np.nan),
+                                                          where=rebinned_count_by_pa_and_gyro != 0)
+    pa_only_with_delta_plus = np.divide(rebinned_summed_pa_only_with_delta_plus,
+                                        rebinned_count_pa_only,
+                                        out=np.full_like(
+                                            rebinned_summed_pa_only_with_delta_plus, np.nan),
+                                        where=rebinned_count_pa_only != 0)
+    pa_only_with_delta_minus = np.divide(rebinned_summed_pa_only_with_delta_minus,
+                                         rebinned_count_pa_only,
+                                         out=np.full_like(
+                                             rebinned_summed_pa_only_with_delta_minus, np.nan),
+                                         where=rebinned_count_pa_only != 0)
 
     return (nominal_values(averaged_rebinned_fluxes_with_delta_plus),
             std_devs(averaged_rebinned_fluxes_with_delta_plus),

imap_l3_processing/pitch_angles.py
@@ -13,9 +13,9 @@ def calculate_unit_vector(vector: np.ndarray[float]) -> np.ndarray[float]:
 
 
 def rotate_from_imap_despun_to_hit_despun(vector: np.ndarray[float]) -> np.ndarray[float]:
-    rotation_matrix_from_imap_despun_frame_to_hit_despun_instrument_frame = [[[0.866025, 0.5, 0],
-                                                                              [-0.5, 0.866025, 0],
-                                                                              [0, 0, 1]]]
+    rotation_matrix_from_imap_despun_frame_to_hit_despun_instrument_frame = [[0.866025, 0.5, 0],
+                                                                             [-0.5, 0.866025, 0],
+                                                                             [0, 0, 1]]
     return rotation_matrix_from_imap_despun_frame_to_hit_despun_instrument_frame @ vector
 
 

tests/hit/l3/sectored_products/science/test_sectored_products_algorithms.py
@@ -58,7 +58,7 @@ class TestSectoredProductsAlgorithms(TestCase):
                  [[4, 0, 1, 5], [7, 3, 2, 6]],
                  [[12, 8, 9, 13], [15, 11, 10, 14]]
              ],
-             [[2.5, 4.5], [10.5, 12.5]]),
+             [[2.5, 4.5], [10.5, 12.5]])
         ]
 
         for case, pitch_angles, gyrophases, number_of_pitch_angle_bins, number_of_gyrophase_bins, \
@@ -76,6 +76,21 @@ class TestSectoredProductsAlgorithms(TestCase):
 
                 np.testing.assert_equal(fluxes_pa_and_gyro, expected_flux_by_pa_gyrophase)
                 np.testing.assert_equal(fluxes_pa_only, expected_flux_by_pa)
+
+    def test_rebin_by_pitch_angle_and_gyrophase_pa_product_only_correctly_weights_when_averaging(self):
+        fluxes = np.array([[[0, 1, 2], [3, 4, 5]]])
+        rebinned_data = rebin_by_pitch_angle_and_gyrophase(
+            fluxes,
+            fluxes * 0.01,
+            fluxes * 0.01,
+            np.array([[45, 45, 45], [135, 135, 135]]),
+            np.array([[45, 135, 225], [45, 135, 225]]),
+            1, 2)
+        fluxes_pa_and_gyro, _, _ = rebinned_data[0:3]
+        fluxes_pa_only, _, _ = rebinned_data[3:6]
+
+        np.testing.assert_equal(fluxes_pa_and_gyro, [[[2, 3.5]]])
+        np.testing.assert_equal(fluxes_pa_only, [[2.5]])
 
     def test_rebin_by_pitch_angle_and_gyrophase_includes_uncertainties(self):
         default_pitch_angles = np.array([[45, 45, 45, 45], [135, 135, 135, 135]])

tests/test_pitch_angles.py
@@ -44,7 +44,7 @@ class TestPitchAngles(unittest.TestCase):
 
         rotated_vector = rotate_from_imap_despun_to_hit_despun(vector)
 
-        expected_vector = np.array([[1.366025, 0.366025, 1.0]])
+        expected_vector = np.array([1.366025, 0.366025, 1.0])
 
         np.testing.assert_array_almost_equal(rotated_vector, expected_vector)
 