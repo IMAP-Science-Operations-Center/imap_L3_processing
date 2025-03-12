27cf3158e8af608808fc9eac3f3dc63f7f65c75f
Pleasant <pleasant@menloinnovations.com>
2025-03-12 16:59:16 -0400

    Harrison 2419 - KJON/GLUN - Extracted spectral fit into science layer.

---
+++ imap_processing/hi/l3/science/spectral_fit.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from imap_processing.hi.l3.science.mpfit import mpfit
+
+
+def spectral_fit(num_epochs, num_lons, num_lats, fluxes, variances, energy):
+    initial_parameters = (1, 1)
+
+    gammas = np.full((num_epochs, num_lons, num_lats), fill_value=np.nan, dtype=float)
+    for epoch in range(num_epochs):
+        for lon in range(num_lons):
+            for lat in range(num_lats):
+                flux = fluxes[epoch][lon][lat]
+                variance = variances[epoch][lon][lat]
+                keywords = {'xval': energy, 'yval': flux, 'errval': variance}
+                _, gamma = mpfit(power_law, initial_parameters, keywords, nprint=0, maxiter=50).params
+                gammas[epoch][lon][lat] = gamma
+    return gammas
+
+
+def power_law(params, **kwargs):
+    A, B = params
+    x = kwargs['xval']
+    y = kwargs['yval']
+    err = kwargs['errval']
+
+    model = A * np.power(x, -B)
+
+    status = 0
+    residuals = (y - model) / err
+    # print(f'A: {A}\nB: {B}\nresiduals: {residuals}')
+
+    return status, residuals
---
+++ tests/hi/l3/science/test_spectral_fit.py
@@ -0,0 +1,38 @@
+import unittest
+
+import numpy as np
+
+from imap_processing.hi.l3.science.spectral_fit import power_law, spectral_fit
+from imap_processing.models import InputMetadata
+
+
+class TestHiProcessor(unittest.TestCase):
+    def test_power_law_function(self):
+        params = (2, -2)
+        x = np.array([1, 2, 3])
+        y = np.array([4, 10, 22])
+        err = np.array([2, 2, 2])
+        keywords = {'xval': x, 'yval': y, 'errval': err}
+
+        expected_residual = np.array([1, 1, 2])
+        status, actual_residuals = power_law(params, **keywords)
+
+        np.testing.assert_array_equal(actual_residuals, expected_residual)
+        self.assertEqual(status, 0)
+
+    def test_finds_best_fit(self):
+        np.random.seed(42)
+        energies = np.geomspace(1, 10, 23)
+        true_A, true_gamma = 2.0, -1.5
+        flux_data = true_A * np.power(energies, -true_gamma)
+
+        errors = 0.2 * np.abs(flux_data)
+
+        num_lats = 1
+        num_lons = 1
+        num_epochs = 1
+        flux = np.array(flux_data).reshape(1, 1, 1, len(energies))
+        variance = np.array(errors).reshape(1, 1, 1, len(energies))
+
+        result = spectral_fit(num_epochs, num_lons, num_lats, flux, variance, energies)
+        np.testing.assert_array_equal(result, np.array(true_gamma).reshape(1, 1, 1))
