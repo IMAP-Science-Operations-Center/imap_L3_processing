bd7c6324cfb99235f0ac111002adea3370431853
Pleasant <pleasant@menloinnovations.com>
2025-03-17 16:00:59 -0400

    Harrison 2419 - KJON/GLUN - Handles fill values in input data set for Hi.

--- imap_l3_processing/hi/l3/utils.py
+++ imap_l3_processing/hi/l3/utils.py
@@ -1,21 +1,22 @@
 from spacepy.pycdf import CDF
 
+from imap_l3_processing.cdf.cdf_utils import read_variable
 from imap_l3_processing.hi.l3.models import HiL3Data
 
 
-def read_hi_l3_data(cdf_path) -> HiL3Data:
+def read_hi_l2_data(cdf_path) -> HiL3Data:
     with CDF(str(cdf_path)) as cdf:
         return HiL3Data(
             epoch=cdf["Epoch"][...],
             energy=cdf["bin"][...],
             energy_deltas=cdf["bin_boundaries"][...],
-            counts=cdf["counts"][...],
-            counts_uncertainty=cdf["counts_uncertainty"][...],
+            counts=read_variable(cdf["counts"]),
+            counts_uncertainty=read_variable(cdf["counts_uncertainty"]),
             epoch_delta=cdf["epoch_delta"][...],
-            exposure=cdf["exposure"][...],
-            flux=cdf["flux"][...],
+            exposure=read_variable(cdf["exposure"]),
+            flux=read_variable(cdf["flux"]),
             lat=cdf["lat"][...],
             lon=cdf["lon"][...],
-            sensitivity=cdf["sensitivity"][...],
-            variance=cdf["variance"][...]
+            sensitivity=read_variable(cdf["sensitivity"]),
+            variance=read_variable(cdf["variance"])
         )
