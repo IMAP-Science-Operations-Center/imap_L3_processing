650f4a7ae529f83f29822ba00091b4956f908a10
Pleasant <pleasant@menloinnovations.com>
2025-03-27 14:22:24 -0400

    Harrison 2419 - KJON/SWYN - Added init files for hi/l3 and hi/l3/science. Changed patch to read_hi_l3_data to the actual read_hi_l2_data function in prod.

--- tests/hi/l3/test_hi_l3_dependencies.py
+++ tests/hi/l3/test_hi_l3_dependencies.py
@@ -8,28 +8,28 @@ from imap_l3_processing.models import UpstreamDataDependency
 
 class TestHiL3Dependencies(unittest.TestCase):
 
-    @patch("imap_l3_processing.hi.l3.hi_l3_dependencies.read_hi_l3_data")
-    def test_from_file_paths(self, read_hi_l3_data):
+    @patch("imap_l3_processing.hi.l3.hi_l3_dependencies.read_hi_l2_data")
+    def test_from_file_paths(self, read_hi_l2_data):
         hi_l3 = Path("test_hi_l3_cdf.cdf")
 
         result = HiL3Dependencies.from_file_paths(hi_l3)
 
-        read_hi_l3_data.assert_called_with(hi_l3)
+        read_hi_l2_data.assert_called_with(hi_l3)
 
-        self.assertEqual(read_hi_l3_data.call_count, 1)
-        self.assertEqual(read_hi_l3_data.return_value, result.hi_l3_data)
+        self.assertEqual(read_hi_l2_data.call_count, 1)
+        self.assertEqual(read_hi_l2_data.return_value, result.hi_l3_data)
 
     @patch("imap_l3_processing.hi.l3.hi_l3_dependencies.download_dependency")
-    @patch("imap_l3_processing.hi.l3.hi_l3_dependencies.read_hi_l3_data")
-    def test_fetch_dependencies(self, mock_read_hi_l3_data, mock_download_dependency):
+    @patch("imap_l3_processing.hi.l3.hi_l3_dependencies.read_hi_l2_data")
+    def test_fetch_dependencies(self, mock_read_hi_l2_data, mock_download_dependency):
         hi_l3_dependency = UpstreamDataDependency("hi", "l3", None, None, "latest", "spectral-fit-index")
         dependencies = [hi_l3_dependency]
 
         dependency = HiL3Dependencies.fetch_dependencies(dependencies)
 
         mock_download_dependency.assert_called_with(hi_l3_dependency)
-        mock_read_hi_l3_data.assert_called_with(mock_download_dependency.return_value)
-        self.assertEqual(mock_read_hi_l3_data.return_value, dependency.hi_l3_data)
+        mock_read_hi_l2_data.assert_called_with(mock_download_dependency.return_value)
+        self.assertEqual(mock_read_hi_l2_data.return_value, dependency.hi_l3_data)
 
     @patch("imap_l3_processing.hi.l3.hi_l3_dependencies.download_dependency")
     def test_raises_value_error_if_instrument_doesnt_match(self, _):
