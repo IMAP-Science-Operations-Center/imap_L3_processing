cad842bfb0e47055b79b7dff78cace4afa4b9a2c
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-03-31 11:26:45 -0400

    Harrison 2383 - TLAY/GSTU - update tests for creating yaml file

--- tests/scripts/test_convert_product_definition_to_yaml.py
+++ tests/scripts/test_convert_product_definition_to_yaml.py
@@ -8,9 +8,8 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
 
     def test_parse_required_for_data_with_multiple_depend(self):
         required_for_data = ["NAME", "DATA_TYPE", "CATDESC", "VAR_TYPE", "DEPEND_0", "DEPEND_1", "DEPEND_2", "DEPEND_3",
-                             "DISPLAY_TYPE",
-                             "FIELDNAM", "FORMAT", "LABLAXIS", "UNITS", "VALIDMIN", "VALIDMAX", "FILLVAL", "LABL_PTRS",
-                             "UNIT_PTR", "VARIABLE_PURPOSE"]
+                             "DISPLAY_TYPE", "FIELDNAM", "FORMAT", "UNITS", "VALIDMIN", "VALIDMAX", "FILLVAL",
+                             "LABL_PTR_1", "LABL_PTR_2", "LABL_PTR_3", "VARIABLE_PURPOSE"]
 
         test_csv_values = [
             "intensity_by_pitch_angle_and_gyrophase",
@@ -24,13 +23,13 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
             "spectrogram",
             "Electron Intensity",
             "F9.3",
-            "",
             "cm^-2 sr^-1 s^-1 eV^-1",
             "1E-40",
             "",
             "-1.00E+31",
-            "",
-            "",
+            "energy",
+            "pitch_angle",
+            "gyrophase",
             "primary var, summary",
         ]
 
@@ -52,20 +51,20 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
    DISPLAY_TYPE: spectrogram
    FIELDNAM: Electron Intensity
    FORMAT: F9.3
-   LABLAXIS: ' '
    UNITS: cm^-2 sr^-1 s^-1 eV^-1
    VALIDMIN: 1E-40
    VALIDMAX: ' '
    FILLVAL: -1.00E+31
-   LABL_PTRS: ' '
-   VARIABLE_PURPOSE: primary var, summary
-"""
+   LABL_PTR_1: energy
+   LABL_PTR_2: pitch_angle
+   LABL_PTR_3: gyrophase
+   VARIABLE_PURPOSE: primary var, summary"""
         actual_yaml = convert_csv_to_yaml(filePath)
 
         self.assertEqual(expected_yaml, actual_yaml)
 
     def test_parse_required_for_data_only_depend_0(self):
-        required_for_data = ["NAME", "DATA_TYPE", "CATDESC", "VAR_TYPE", "DEPEND_0", "DISPLAY_TYPE",
+        required_for_data = ["NAME", "DATA_TYPE", "CATDESC", "VAR_TYPE", "RECORD_VARYING", "DEPEND_0", "DISPLAY_TYPE",
                              "FIELDNAM", "FORMAT", "LABLAXIS", "UNITS", "VALIDMIN", "VALIDMAX", "FILLVAL"]
 
         test_csv_values = [
@@ -73,6 +72,7 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
             "float32",
             "Number of energies used on core fit",
             "support_data",
+            "RV",
             "epoch",
             "no_plot",
             "",
@@ -94,16 +94,96 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
    DATA_TYPE: float32
    CATDESC: Number of energies used on core fit
    VAR_TYPE: support_data
+   RECORD_VARYING: RV
    DEPEND_0: epoch
-   DISPLAY_TYPE: no_plot
    FIELDNAM: ' '
    FORMAT: ' '
    LABLAXIS: ' '
    UNITS: ' '
    VALIDMIN: ' '
    VALIDMAX: ' '
-   FILLVAL: -1.00E+31
-"""
+   FILLVAL: -1.00E+31"""
+        actual_yaml = convert_csv_to_yaml(filePath)
+        self.assertEqual(expected_yaml, actual_yaml)
+
+    def test_parse_required_for_support_data_non_rv(self):
+        required_for_data = ["NAME", "DATA_TYPE", "CATDESC", "VAR_TYPE", "RECORD_VARYING", "DISPLAY_TYPE",
+                             "FIELDNAM", "FORMAT", "LABLAXIS", "UNITS", "VALIDMIN", "VALIDMAX", "FILLVAL"]
+
+        test_csv_values = [
+            "energy",
+            "float32",
+            "energy bins",
+            "support_data",
+            "NRV",
+            "no_plot",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "-1.00E+31"
+        ]
+        filePath = "csv_test_file.csv"
+        with open(filePath, "w", newline='') as csvfile:
+            test_csv_writer = csv.writer(csvfile)
+            test_csv_writer.writerow(required_for_data)
+            test_csv_writer.writerow(test_csv_values)
+
+            expected_yaml = """energy:
+   NAME: energy
+   DATA_TYPE: float32
+   CATDESC: energy bins
+   VAR_TYPE: support_data
+   RECORD_VARYING: NRV
+   FIELDNAM: ' '
+   FORMAT: ' '
+   LABLAXIS: ' '
+   UNITS: ' '
+   VALIDMIN: ' '
+   VALIDMAX: ' '
+   FILLVAL: -1.00E+31"""
+        actual_yaml = convert_csv_to_yaml(filePath)
+        self.assertEqual(expected_yaml, actual_yaml)
+
+    def test_parse_required_for_data_epoch(self):
+        required_for_data = ["NAME", "DATA_TYPE", "CATDESC", "VAR_TYPE", "DEPEND_0", "DISPLAY_TYPE",
+                             "FIELDNAM", "FORMAT", "LABLAXIS", "UNITS", "VALIDMIN", "VALIDMAX", "FILLVAL"]
+
+        test_csv_values = [
+            "epoch",
+            "float32",
+            "The time",
+            "support_data",
+            "",
+            "no_plot",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "-1.00E+31"
+        ]
+        filePath = "csv_test_file.csv"
+        with open(filePath, "w", newline='') as csvfile:
+            test_csv_writer = csv.writer(csvfile)
+            test_csv_writer.writerow(required_for_data)
+            test_csv_writer.writerow(test_csv_values)
+
+            expected_yaml = """epoch:
+   NAME: epoch
+   DATA_TYPE: float32
+   CATDESC: The time
+   VAR_TYPE: support_data
+   FIELDNAM: ' '
+   FORMAT: ' '
+   LABLAXIS: ' '
+   UNITS: ' '
+   VALIDMIN: ' '
+   VALIDMAX: ' '
+   FILLVAL: -1.00E+31"""
         actual_yaml = convert_csv_to_yaml(filePath)
         self.assertEqual(expected_yaml, actual_yaml)
 
@@ -113,6 +193,7 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
                        "Data shape",
                        "CATDESC",
                        "VAR_TYPE",
+                       "RECORD_VARYING",
                        "DEPEND_0",
                        "DEPEND_1",
                        "DEPEND_2",
@@ -155,6 +236,7 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
             "(epoch, stim_tag)",
             "Stim Tag (stimulus event)",
             "support_data",
+            "RV",
             "epoch",
             "",
             "",
@@ -202,8 +284,8 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
    DATA_TYPE: int8
    CATDESC: Stim Tag (stimulus event)
    VAR_TYPE: support_data
+   RECORD_VARYING: RV
    DEPEND_0: epoch
-   DISPLAY_TYPE: no_plot
    FIELDNAM: stim_tag
    FORMAT: ' '
    LABLAXIS: ' '
@@ -211,9 +293,7 @@ class TestConvertProductDefinitionToYamlTest(unittest.TestCase):
    VALIDMIN: 0
    VALIDMAX: 1
    FILLVAL: -1.28E+02
-   LABL_PTRS: ' '
-   SCALE_TYP: linear
-"""
+   SCALE_TYP: linear"""
 
         actual_yaml = convert_csv_to_yaml(filePath)
         self.assertEqual(expected_yaml, actual_yaml)
