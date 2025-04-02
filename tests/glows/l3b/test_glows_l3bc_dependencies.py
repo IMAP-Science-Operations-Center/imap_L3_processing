7513c00ea558bc7709e55376eb7b6e0d8ee09b74
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-04-02 09:56:21 -0400

    Harrison 2523 - PMCL/GLUN - Renames GLOWS L3b folder to L3bc

--- tests/glows/l3b/test_glows_l3bc_dependencies.py
+++ tests/glows/l3b/test_glows_l3bc_dependencies.py
@@ -2,17 +2,17 @@ import unittest
 from pathlib import Path
 from unittest.mock import patch, sentinel, call, Mock, MagicMock, mock_open
 
-from imap_l3_processing.glows.l3b import glows_l3bc_dependencies
-from imap_l3_processing.glows.l3b.glows_l3bc_dependencies import GlowsL3BCDependencies
+from imap_l3_processing.glows.l3bc import glows_l3bc_dependencies
+from imap_l3_processing.glows.l3bc.glows_l3bc_dependencies import GlowsL3BCDependencies
 
 
 class TestGlowsL3BCDependencies(unittest.TestCase):
 
     @patch('builtins.open', new_callable=mock_open, create=False)
-    @patch('imap_l3_processing.glows.l3b.glows_l3bc_dependencies.ZipFile')
-    @patch('imap_l3_processing.glows.l3b.glows_l3bc_dependencies.CDF')
-    @patch('imap_l3_processing.glows.l3b.glows_l3bc_dependencies.download_dependency')
-    @patch('imap_l3_processing.glows.l3b.glows_l3bc_dependencies.download_dependency_from_path')
+    @patch('imap_l3_processing.glows.l3bc.glows_l3bc_dependencies.ZipFile')
+    @patch('imap_l3_processing.glows.l3bc.glows_l3bc_dependencies.CDF')
+    @patch('imap_l3_processing.glows.l3bc.glows_l3bc_dependencies.download_dependency')
+    @patch('imap_l3_processing.glows.l3bc.glows_l3bc_dependencies.download_dependency_from_path')
     def test_fetch_dependencies(self, mock_download_dependencies_from_path, mock_download_dependencies,
                                 mock_cdf_constructor, mock_zip_file_class,
                                 mock_open_file):
