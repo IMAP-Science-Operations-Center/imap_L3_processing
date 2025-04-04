17a615b1a1cb5d6f60689c4dd33fde0f683a6477
Pleasant <pleasant@menloinnovations.com>
2025-04-04 16:40:45 -0400

    Harrison 2537 - KJON/SWYN - created some fake L1c data and L2 map CDFs that can be used to test the survival probability correction code

---
+++ scripts/hi/generate_fake_l1c_cdfs.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta
+
+import numpy as np
+from spacepy.pycdf import CDF
+
+from tests.test_helpers import get_test_data_path
+
+num_fake_files = 90
+
+for i in range(num_fake_files):
+    date_to_set = datetime(month=4, year=2025, day=16, hour=12) + timedelta(days=i)
+    logical_source = f"imap_hi_l1c_45sensor-pset_{date_to_set.strftime('%Y%m%d')}_v001.cdf"
+    output_filename = str(get_test_data_path(f"hi/fake_l1c/{logical_source}.cdf"))
+    with CDF(output_filename,
+             masterpath=str(get_test_data_path("hi/imap_hi_l1c_45sensor-pset_20250415_v001.cdf"))) as cdf:
+        cdf['epoch'] = np.array([date_to_set])
