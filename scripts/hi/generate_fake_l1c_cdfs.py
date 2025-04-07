a4a16316b1c8984458cfc728ce022cd11a839892
Pleasant <pleasant@menloinnovations.com>
2025-04-07 16:13:48 -0400

    Harrison 2537 - PSCH/SWYN - Created fake GLOWS L3e and HI L1c files that can be used for testing the survival probability correction

--- scripts/hi/generate_fake_l1c_cdfs.py
+++ scripts/hi/generate_fake_l1c_cdfs.py
@@ -9,7 +9,7 @@ num_fake_files = 90
 
 for i in range(num_fake_files):
     date_to_set = datetime(month=4, year=2025, day=16, hour=12) + timedelta(days=i)
-    logical_source = f"imap_hi_l1c_45sensor-pset_{date_to_set.strftime('%Y%m%d')}_v001.cdf"
+    logical_source = f"imap_hi_l1c_45sensor-pset_{date_to_set.strftime('%Y%m%d')}_v001"
     output_filename = str(get_test_data_path(f"hi/fake_l1c/{logical_source}.cdf"))
     with CDF(output_filename,
              masterpath=str(get_test_data_path("hi/imap_hi_l1c_45sensor-pset_20250415_v001.cdf"))) as cdf:
