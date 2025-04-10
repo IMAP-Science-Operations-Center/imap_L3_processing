3ce4c72bf267ce834e54924b3029ea63cb18db6b
Pleasant <pleasant@menloinnovations.com>
2025-04-10 16:42:14 -0400

    Harrison 2537 - PSCH/PMCL - Prevent TestSwapiProcessor from removing temp_cdf_data

--- tests/swapi/test_swapi_processor.py
+++ tests/swapi/test_swapi_processor.py
@@ -20,15 +20,10 @@ from imap_l3_processing.swapi.swapi_processor import SwapiProcessor
 
 class TestSwapiProcessor(TestCase):
     def setUp(self) -> None:
-        self.temp_directory = TEMP_CDF_FOLDER_PATH
-        if os.path.exists(self.temp_directory):
-            shutil.rmtree(self.temp_directory)
-        os.mkdir(self.temp_directory)
         self.mock_imap_patcher = patch('imap_l3_processing.swapi.swapi_processor.imap_data_access')
         self.mock_imap_api = self.mock_imap_patcher.start()
 
     def tearDown(self) -> None:
-        shutil.rmtree(self.temp_directory)
         self.mock_imap_patcher.stop()
 
     @patch('imap_l3_processing.swapi.swapi_processor.spice_wrapper.furnish')
@@ -176,7 +171,7 @@ class TestSwapiProcessor(TestCase):
                 input_metadata.descriptor = descriptor_to_generate
 
                 expected_cdf_path = str(
-                    self.temp_directory / f"imap_swapi_l3a_{descriptor_to_generate}_{start_date_as_str}_{input_version}.cdf")
+                    TEMP_CDF_FOLDER_PATH / f"imap_swapi_l3a_{descriptor_to_generate}_{start_date_as_str}_{input_version}.cdf")
 
                 mock_chunk_l2_data.side_effect = [
                     [chunk_of_five],
