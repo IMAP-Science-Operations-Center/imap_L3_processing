934f4b16a0d39c650f4c5742d66cb93eed0788c5
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-04-14 15:43:17 -0400

    Harrison 2419 - BWYN/GLUN - Update metadata for spectral index data product
    Updated metadata csv and yaml files for spectral fit index data product. Added non-record varying to lon, lat, energy, and energy delta variables.

--- imap_l3_processing/hi/l3/models.py
+++ imap_l3_processing/hi/l3/models.py
@@ -7,15 +7,15 @@ from spacepy import pycdf
 from imap_l3_processing.models import InputMetadata, DataProduct, DataProductVariable
 
 EPOCH_VAR_NAME = "epoch"
+EPOCH_DELTA_VAR_NAME = "epoch_delta"
 LAT_VAR_NAME = "lat"
 LONG_VAR_NAME = "lon"
 ENERGY_VAR_NAME = "energy"
-FLUX_VAR_NAME = "flux"
-VARIANCE_VAR_NAME = "variance"
 ENERGY_DELTAS_VAR_NAME = "energy_deltas"
+INTENSITY_VAR_NAME = "intensity"
+VARIANCE_VAR_NAME = "variance"
 COUNTS_VAR_NAME = "counts"
 COUNTS_UNCERTAINTY_VAR_NAME = "counts_uncertainty"
-EPOCH_DELTA_VAR_NAME = "epoch_delta"
 EXPOSURE_VAR_NAME = "exposure"
 SENSITIVITY_VAR_NAME = "sensitivity"
 SPECTRAL_FIT_INDEX_VAR_NAME = "spectral_fit_index"
@@ -49,12 +49,16 @@ class HiL3SpectralIndexDataProduct(DataProduct, HiMapData):
     def to_data_product_variables(self) -> list[DataProductVariable]:
         return [
             DataProductVariable(EPOCH_VAR_NAME, self.epoch, cdf_data_type=pycdf.const.CDF_TIME_TT2000),
-            DataProductVariable(LAT_VAR_NAME, self.lat, cdf_data_type=pycdf.const.CDF_DOUBLE),
-            DataProductVariable(LONG_VAR_NAME, self.lon, cdf_data_type=pycdf.const.CDF_DOUBLE),
-            DataProductVariable(ENERGY_VAR_NAME, self.energy, cdf_data_type=pycdf.const.CDF_DOUBLE),
-            DataProductVariable(FLUX_VAR_NAME, self.flux, cdf_data_type=pycdf.const.CDF_DOUBLE),
+            DataProductVariable(LAT_VAR_NAME, self.lat, cdf_data_type=pycdf.const.CDF_DOUBLE,
+                                record_varying=False),
+            DataProductVariable(LONG_VAR_NAME, self.lon, cdf_data_type=pycdf.const.CDF_DOUBLE,
+                                record_varying=False),
+            DataProductVariable(ENERGY_VAR_NAME, self.energy, cdf_data_type=pycdf.const.CDF_DOUBLE,
+                                record_varying=False),
+            DataProductVariable(ENERGY_DELTAS_VAR_NAME, self.energy_deltas, cdf_data_type=pycdf.const.CDF_DOUBLE,
+                                record_varying=False),
+            DataProductVariable(INTENSITY_VAR_NAME, self.flux, cdf_data_type=pycdf.const.CDF_DOUBLE),
             DataProductVariable(VARIANCE_VAR_NAME, self.variance, cdf_data_type=pycdf.const.CDF_DOUBLE),
-            DataProductVariable(ENERGY_DELTAS_VAR_NAME, self.energy_deltas, cdf_data_type=pycdf.const.CDF_DOUBLE),
             DataProductVariable(COUNTS_VAR_NAME, self.counts, cdf_data_type=pycdf.const.CDF_DOUBLE),
             DataProductVariable(COUNTS_UNCERTAINTY_VAR_NAME, self.counts_uncertainty,
                                 cdf_data_type=pycdf.const.CDF_DOUBLE),
@@ -79,7 +83,7 @@ class HiL3SurvivalCorrectedDataProduct(DataProduct, HiMapData):
             DataProductVariable(COUNTS_UNCERTAINTY_VAR_NAME, self.counts_uncertainty),
             DataProductVariable(EPOCH_DELTA_VAR_NAME, self.epoch_delta),
             DataProductVariable(EXPOSURE_VAR_NAME, self.exposure),
-            DataProductVariable(FLUX_VAR_NAME, self.flux),
+            DataProductVariable(INTENSITY_VAR_NAME, self.flux),
             DataProductVariable(LAT_VAR_NAME, self.lat),
             DataProductVariable(LONG_VAR_NAME, self.lon),
             DataProductVariable(SENSITIVITY_VAR_NAME, self.sensitivity),
