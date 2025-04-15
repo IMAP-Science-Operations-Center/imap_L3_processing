83afeb67b38181aa833722bf2e9c31209307a98c
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-04-15 16:57:54 -0400

    Harrison 2556 - PSCH/MDEB - HIT: Make macropixel output more ISTP compliant

--- imap_l3_processing/hit/l3/sectored_products/models.py
+++ imap_l3_processing/hit/l3/sectored_products/models.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 import numpy as np
-from spacepy import pycdf
 
 from imap_l3_processing.models import DataProduct, DataProductVariable
 
@@ -58,6 +57,13 @@ IRON_ENERGY_DELTA_PLUS_CDF_VAR_NAME = "fe_energy_delta_plus"
 IRON_ENERGY_DELTA_MINUS_CDF_VAR_NAME = "fe_energy_delta_minus"
 MEASUREMENT_PITCH_ANGLE_VAR_NAME = "measurement_pitch_angle"
 MEASUREMENT_GYROPHASE_VAR_NAME = "measurement_gyrophase"
+PITCH_ANGLE_LABEL_VAR_NAME = "pitch_angle_label"
+GYROPHASE_LABEL_VAR_NAME = "gyrophase_label"
+H_ENERGY_LABEL_VAR_NAME = "h_energy_label"
+HE4_ENERGY_LABEL_VAR_NAME = "he4_energy_label"
+CNO_ENERGY_LABEL_VAR_NAME = "cno_energy_label"
+NE_MG_SI_ENERGY_LABEL_VAR_NAME = "nemgsi_energy_label"
+IRON_ENERGY_LABEL_VAR_NAME = "fe_energy_label"
 
 
 @dataclass
@@ -118,62 +124,72 @@ class HitPitchAngleDataProduct(DataProduct):
 
     def to_data_product_variables(self) -> list[DataProductVariable]:
         return [
-            DataProductVariable(EPOCH_CDF_VAR_NAME, self.epochs, cdf_data_type=pycdf.const.CDF_TIME_TT2000),
+            DataProductVariable(EPOCH_CDF_VAR_NAME, self.epochs),
             DataProductVariable(EPOCH_DELTA_CDF_VAR_NAME,
-                                np.array([t.total_seconds() for t in self.epoch_deltas]) * 1e9,
-                                cdf_data_type=pycdf.const.CDF_INT8),
-            DataProductVariable(PITCH_ANGLE_CDF_VAR_NAME, self.pitch_angles, record_varying=False),
-            DataProductVariable(PITCH_ANGLE_DELTA_CDF_VAR_NAME, self.pitch_angle_deltas, record_varying=False),
-            DataProductVariable(GYROPHASE_CDF_VAR_NAME, self.gyrophases, record_varying=False),
-            DataProductVariable(GYROPHASE_DELTA_CDF_VAR_NAME, self.gyrophase_deltas, record_varying=False),
+                                np.array([t.total_seconds() for t in self.epoch_deltas]) * 1e9),
+            DataProductVariable(PITCH_ANGLE_CDF_VAR_NAME, self.pitch_angles),
+            DataProductVariable(PITCH_ANGLE_DELTA_CDF_VAR_NAME, self.pitch_angle_deltas),
+            DataProductVariable(GYROPHASE_CDF_VAR_NAME, self.gyrophases),
+            DataProductVariable(GYROPHASE_DELTA_CDF_VAR_NAME, self.gyrophase_deltas),
             DataProductVariable(H_INTENSITY_CDF_VAR_NAME, self.h_intensity),
             DataProductVariable(H_INTENSITY_DELTA_PLUS_CDF_VAR_NAME, self.h_intensity_delta_plus),
             DataProductVariable(H_INTENSITY_DELTA_MINUS_CDF_VAR_NAME, self.h_intensity_delta_minus),
             DataProductVariable(H_INTENSITY_PA_CDF_VAR_NAME, self.h_intensity_pa),
             DataProductVariable(H_INTENSITY_PA_DELTA_PLUS_CDF_VAR_NAME, self.h_intensity_pa_delta_plus),
             DataProductVariable(H_INTENSITY_PA_DELTA_MINUS_CDF_VAR_NAME, self.h_intensity_pa_delta_minus),
-            DataProductVariable(H_ENERGY_CDF_VAR_NAME, self.h_energies, record_varying=False),
-            DataProductVariable(H_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.h_energy_delta_plus, record_varying=False),
-            DataProductVariable(H_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.h_energy_delta_minus, record_varying=False),
+            DataProductVariable(H_ENERGY_CDF_VAR_NAME, self.h_energies),
+            DataProductVariable(H_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.h_energy_delta_plus),
+            DataProductVariable(H_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.h_energy_delta_minus),
             DataProductVariable(HE4_INTENSITY_CDF_VAR_NAME, self.he4_intensity),
             DataProductVariable(HE4_INTENSITY_DELTA_PLUS_CDF_VAR_NAME, self.he4_intensity_delta_plus),
             DataProductVariable(HE4_INTENSITY_DELTA_MINUS_CDF_VAR_NAME, self.he4_intensity_delta_minus),
             DataProductVariable(HE4_INTENSITY_PA_CDF_VAR_NAME, self.he4_intensity_pa),
             DataProductVariable(HE4_INTENSITY_PA_DELTA_PLUS_CDF_VAR_NAME, self.he4_intensity_pa_delta_plus),
             DataProductVariable(HE4_INTENSITY_PA_DELTA_MINUS_CDF_VAR_NAME, self.he4_intensity_pa_delta_minus),
-            DataProductVariable(HE4_ENERGY_CDF_VAR_NAME, self.he4_energies, record_varying=False),
-            DataProductVariable(HE4_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.he4_energy_delta_plus, record_varying=False),
-            DataProductVariable(HE4_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.he4_energy_delta_minus, record_varying=False),
+            DataProductVariable(HE4_ENERGY_CDF_VAR_NAME, self.he4_energies),
+            DataProductVariable(HE4_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.he4_energy_delta_plus),
+            DataProductVariable(HE4_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.he4_energy_delta_minus),
             DataProductVariable(CNO_INTENSITY_CDF_VAR_NAME, self.cno_intensity),
             DataProductVariable(CNO_INTENSITY_DELTA_PLUS_CDF_VAR_NAME, self.cno_intensity_delta_plus),
             DataProductVariable(CNO_INTENSITY_DELTA_MINUS_CDF_VAR_NAME, self.cno_intensity_delta_minus),
             DataProductVariable(CNO_INTENSITY_PA_CDF_VAR_NAME, self.cno_intensity_pa),
             DataProductVariable(CNO_INTENSITY_PA_DELTA_PLUS_CDF_VAR_NAME, self.cno_intensity_pa_delta_plus),
             DataProductVariable(CNO_INTENSITY_PA_DELTA_MINUS_CDF_VAR_NAME, self.cno_intensity_pa_delta_minus),
-            DataProductVariable(CNO_ENERGY_CDF_VAR_NAME, self.cno_energies, record_varying=False),
-            DataProductVariable(CNO_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.cno_energy_delta_plus, record_varying=False),
-            DataProductVariable(CNO_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.cno_energy_delta_minus, record_varying=False),
+            DataProductVariable(CNO_ENERGY_CDF_VAR_NAME, self.cno_energies),
+            DataProductVariable(CNO_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.cno_energy_delta_plus),
+            DataProductVariable(CNO_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.cno_energy_delta_minus),
             DataProductVariable(NE_MG_SI_INTENSITY_CDF_VAR_NAME, self.ne_mg_si_intensity),
             DataProductVariable(NE_MG_SI_INTENSITY_DELTA_PLUS_CDF_VAR_NAME, self.ne_mg_si_intensity_delta_plus),
             DataProductVariable(NE_MG_SI_INTENSITY_DELTA_MINUS_CDF_VAR_NAME, self.ne_mg_si_intensity_delta_minus),
             DataProductVariable(NE_MG_SI_INTENSITY_PA_CDF_VAR_NAME, self.ne_mg_si_intensity_pa),
             DataProductVariable(NE_MG_SI_INTENSITY_PA_DELTA_PLUS_CDF_VAR_NAME, self.ne_mg_si_intensity_pa_delta_plus),
             DataProductVariable(NE_MG_SI_INTENSITY_PA_DELTA_MINUS_CDF_VAR_NAME, self.ne_mg_si_intensity_pa_delta_minus),
-            DataProductVariable(NE_MG_SI_ENERGY_CDF_VAR_NAME, self.ne_mg_si_energies, record_varying=False),
-            DataProductVariable(NE_MG_SI_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.ne_mg_si_energy_delta_plus,
-                                record_varying=False),
-            DataProductVariable(NE_MG_SI_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.ne_mg_si_energy_delta_minus,
-                                record_varying=False),
+            DataProductVariable(NE_MG_SI_ENERGY_CDF_VAR_NAME, self.ne_mg_si_energies),
+            DataProductVariable(NE_MG_SI_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.ne_mg_si_energy_delta_plus),
+            DataProductVariable(NE_MG_SI_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.ne_mg_si_energy_delta_minus),
             DataProductVariable(IRON_INTENSITY_CDF_VAR_NAME, self.iron_intensity),
             DataProductVariable(IRON_INTENSITY_DELTA_PLUS_CDF_VAR_NAME, self.iron_intensity_delta_plus),
             DataProductVariable(IRON_INTENSITY_DELTA_MINUS_CDF_VAR_NAME, self.iron_intensity_delta_minus),
             DataProductVariable(IRON_INTENSITY_PA_CDF_VAR_NAME, self.iron_intensity_pa),
             DataProductVariable(IRON_INTENSITY_PA_DELTA_PLUS_CDF_VAR_NAME, self.iron_intensity_pa_delta_plus),
             DataProductVariable(IRON_INTENSITY_PA_DELTA_MINUS_CDF_VAR_NAME, self.iron_intensity_pa_delta_minus),
-            DataProductVariable(IRON_ENERGY_CDF_VAR_NAME, self.iron_energies, record_varying=False),
-            DataProductVariable(IRON_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.iron_energy_delta_plus, record_varying=False),
-            DataProductVariable(IRON_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.iron_energy_delta_minus,
-                                record_varying=False),
+            DataProductVariable(IRON_ENERGY_CDF_VAR_NAME, self.iron_energies),
+            DataProductVariable(IRON_ENERGY_DELTA_PLUS_CDF_VAR_NAME, self.iron_energy_delta_plus),
+            DataProductVariable(IRON_ENERGY_DELTA_MINUS_CDF_VAR_NAME, self.iron_energy_delta_minus),
             DataProductVariable(MEASUREMENT_PITCH_ANGLE_VAR_NAME, self.measurement_pitch_angle),
-            DataProductVariable(MEASUREMENT_GYROPHASE_VAR_NAME, self.measurement_gyrophase)
+            DataProductVariable(MEASUREMENT_GYROPHASE_VAR_NAME, self.measurement_gyrophase),
+            DataProductVariable(PITCH_ANGLE_LABEL_VAR_NAME,
+                                [f"Pitch Angle Label {str(i + 1)}" for i in range(len(self.pitch_angles))]),
+            DataProductVariable(GYROPHASE_LABEL_VAR_NAME,
+                                [f"Gyrophase Label {str(i + 1)}" for i in range(len(self.gyrophases))]),
+            DataProductVariable(H_ENERGY_LABEL_VAR_NAME,
+                                [f"H Energy Label {str(i + 1)}" for i in range(len(self.h_energies))]),
+            DataProductVariable(HE4_ENERGY_LABEL_VAR_NAME,
+                                [f"He4 Energy Label {str(i + 1)}" for i in range(len(self.he4_energies))]),
+            DataProductVariable(CNO_ENERGY_LABEL_VAR_NAME,
+                                [f"CNO Energy Label {str(i + 1)}" for i in range(len(self.cno_energies))]),
+            DataProductVariable(NE_MG_SI_ENERGY_LABEL_VAR_NAME,
+                                [f"NeMgSi Energy Label {str(i + 1)}" for i in range(len(self.ne_mg_si_energies))]),
+            DataProductVariable(IRON_ENERGY_LABEL_VAR_NAME,
+                                [f"Fe Energy Label {str(i + 1)}" for i in range(len(self.iron_energies))]),
         ]
