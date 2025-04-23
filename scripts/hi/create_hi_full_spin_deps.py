12764e92913e5f40ed4bd45cf64fe16176ce0952
Pleasant <pleasant@menloinnovations.com>
2025-04-23 14:57:00 -0400

    Harrison 2537 - PMCL/SWYN - Hi - Generate data for run_local when needed, in git-ignored directory run_local_input_data

---
+++ scripts/hi/create_hi_full_spin_deps.py
@@ -0,0 +1,73 @@
+import shutil
+from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+from spacepy.pycdf import CDF
+
+from imap_l3_processing.hi.hi_processor import HiProcessor
+from imap_l3_processing.hi.l3.hi_l3_survival_dependencies import HiL3SurvivalDependencies
+from imap_l3_processing.models import InputMetadata
+from imap_l3_processing.utils import save_data
+from scripts.hi.create_cdf_from_instrument_team_data import create_l2_map_from_instrument_team
+from scripts.hi.generate_fake_l1c_cdfs import generate_fake_l1c
+from tests.test_helpers import get_test_instrument_team_data_path, get_test_data_path, get_run_local_data_path
+
+
+def create_hi_full_spin_deps(
+        output_dir: Path = get_run_local_data_path("hi/full_spin_deps"),
+        start_date: datetime = datetime(year=2025, month=4, day=15, hour=12),
+        l2_map_dir: Path = get_test_instrument_team_data_path("hi/hi90-6months"),
+        glows_dir: Path = get_test_data_path("hi/fake_l3e_survival_probabilities/90")):
+    l1c_folder = output_dir / "l1c"
+    l1c_folder.mkdir(parents=True, exist_ok=True)
+
+    l1c_files = [Path(s) for s in generate_fake_l1c(start_date, 185, output_dir=l1c_folder)]
+    original_full_spin_map_path = create_l2_map_from_instrument_team(l2_map_dir, output_dir=output_dir)
+    parents = [f.name for f in l1c_files]
+    glows_files = list(glows_dir.iterdir())
+
+    with CDF(str(original_full_spin_map_path)) as cdf:
+        original_intensity = cdf["ena_intensity"][...]
+
+    ramified_map_deps = HiL3SurvivalDependencies.from_file_paths(original_full_spin_map_path, l1c_files, glows_files,
+                                                                 "h90-sf-ram-hae-4deg-6mo")
+
+    antiramified_map_deps = HiL3SurvivalDependencies.from_file_paths(original_full_spin_map_path, l1c_files,
+                                                                     glows_files,
+                                                                     "h90-sf-anti-hae-4deg-6mo")
+
+    processor = HiProcessor(None, InputMetadata("hi", "l3", None, None, "v001", "h90-sf-sp-hae-4deg-6mo"))
+    ram_data = processor._process_survival_probabilities(ramified_map_deps)
+    ram_exposure_is_zero = np.isnan(ram_data.ena_intensity)
+
+    ram_cdf_path = save_data(ram_data, delete_if_present=True, folder_path=output_dir)
+    ram_logical_source = "imap_hi_l3_h90-sf-ram-hae-4deg-6mo_20250415_v001"
+    with CDF(ram_cdf_path, readonly=False) as ram:
+        ram["exposure_factor"] = np.where(ram_exposure_is_zero, 0, ram_data.exposure_factor)
+        ram["ena_intensity"] = np.where(ram_exposure_is_zero, ram["ena_intensity"].attrs["FILLVAL"],
+                                        original_intensity)
+        ram.attrs["Parents"] = parents
+        ram.attrs["Logical_source"] = ram_logical_source
+        ram["ena_intensity"].attrs["CATDESC"] = ram_logical_source
+
+    shutil.move(ram_cdf_path, output_dir / f"{ram_logical_source}.cdf")
+
+    antiram_data = processor._process_survival_probabilities(antiramified_map_deps)
+    antiram_exposure_is_zero = np.isnan(antiram_data.ena_intensity)
+
+    antiram_cdf_path = save_data(antiram_data, delete_if_present=True, folder_path=output_dir)
+    antiram_logical_source = "imap_hi_l3_h90-sf-anti-hae-4deg-6mo_20250415_v001"
+
+    with CDF(antiram_cdf_path, readonly=False) as antiram:
+        antiram["exposure_factor"] = np.where(antiram_exposure_is_zero, 0, antiram_data.exposure_factor)
+        antiram["ena_intensity"] = np.where(antiram_exposure_is_zero, antiram["ena_intensity"].attrs["FILLVAL"],
+                                            original_intensity)
+        antiram.attrs["Parents"] = parents
+        antiram.attrs["Logical_source"] = antiram_logical_source
+        antiram["ena_intensity"].attrs["CATDESC"] = antiram_logical_source
+    shutil.move(antiram_cdf_path, output_dir / f"{antiram_logical_source}.cdf")
+
+
+if __name__ == '__main__':
+    create_hi_full_spin_deps()
--- scripts/hi/generate_fake_l1c_cdfs.py
+++ scripts/hi/generate_fake_l1c_cdfs.py
@@ -1,17 +1,28 @@
 from datetime import datetime, timedelta
+from pathlib import Path
 
 from spacepy import pycdf
 from spacepy.pycdf import CDF
 
 from tests.test_helpers import get_test_data_path
 
-num_fake_files = 90
 
-for i in range(num_fake_files):
-    date_to_set = datetime(month=4, year=2025, day=16, hour=12) + timedelta(days=i)
-    logical_source = f"imap_hi_l1c_90sensor-pset_{date_to_set.strftime('%Y%m%d')}_v001"
-    output_filename = str(get_test_data_path(f"hi/fake_l1c/90/{logical_source}.cdf"))
-    with CDF(output_filename,
-             masterpath=str(get_test_data_path("hi/imap_hi_l1c_90sensor-pset_20250415_v001.cdf"))) as cdf:
-        del cdf['epoch']
-        cdf.new("epoch", [date_to_set], type=pycdf.const.CDF_TIME_TT2000)
+def generate_fake_l1c(start_date: datetime, num_days: int, output_dir: Path) -> list[str]:
+    output_files = []
+
+    for i in range(num_days):
+        date_to_set = start_date + timedelta(days=i)
+        logical_source = f"imap_hi_l1c_90sensor-pset_{date_to_set.strftime('%Y%m%d')}_v001"
+        output_path = output_dir / f"{logical_source}.cdf"
+        output_path.unlink(missing_ok=True)
+        with CDF(str(output_path),
+                 masterpath=str(get_test_data_path("hi/imap_hi_l1c_90sensor-pset_20250415_v001.cdf"))) as cdf:
+            del cdf['epoch']
+            cdf.new("epoch", [date_to_set], type=pycdf.const.CDF_TIME_TT2000)
+        output_files.append(str(output_path))
+    return output_files
+
+
+if __name__ == "__main__":
+    generate_fake_l1c(datetime(month=4, year=2025, day=16, hour=12), num_days=90,
+                      output_dir=get_test_data_path(f"hi/fake_l1c/90"))
