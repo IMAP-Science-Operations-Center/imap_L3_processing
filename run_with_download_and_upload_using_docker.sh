55ad347a87e18d59348578ccee12d5f8a5d068aa
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-04-24 14:42:13 -0400

    Harrison 2587 - ALOZ/EYEA - Remove usages of UpstreamDataDependency

--- run_with_download_and_upload_using_docker.sh
+++ run_with_download_and_upload_using_docker.sh
@@ -2,9 +2,9 @@ source scripts/update_version.sh
 
 docker run --rm --mount type=bind,src="$(pwd)/spice_kernels",dst="/mnt/spice" \
 --mount type=bind,src="$(pwd)/temp_cdf_data",dst="/temp_cdf_data" \
-$(docker build -q .) --instrument glows --data-level l3b \
---start-date 20100104 --version v002 --dependency \
-'[{"type": "science", "files": ["imap_glows_l3a_hist_20100104_v002.cdf"]}]'
+$(docker build -q .) --instrument swapi --data-level l3a \
+--start-date 20250606 --descriptor proton-sw --version v000 --dependency \
+'[{"type":"science","files":["imap_swapi_l2_sci_20250606_v007.cdf"]},{"type":"ancillary","files":["imap_swapi_density-temperature-lut_20240905_v000.dat"]},{"type":"ancillary","files":["imap_swapi_alpha-density-temperature-lut_20240920_v000.dat"]},{"type":"ancillary","files":["imap_swapi_clock-angle-and-flow-deflection-lut_20240918_v000.dat"]},{"type":"ancillary","files":["imap_swapi_energy-gf-lut_20240923_v000.dat"]},{"type":"ancillary","files":["imap_swapi_instrument-response-lut_20241023_v000.zip"]},{"type":"ancillary","files":["imap_swapi_density-of-neutral-helium-lut_20241023_v000.dat"]}]'
 
 
 git restore imap_l3_processing/version.py
