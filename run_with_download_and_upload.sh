7153f4d6dd9b100a61826cc54f2c204e8d2e80c4
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-04-29 16:24:52 -0400

    Harrison 2537 - BWYN/GLUN/SWYN/PMCL - update run_with_download_and_upload.sh for new descriptor format

--- run_with_download_and_upload.sh
+++ run_with_download_and_upload.sh
@@ -13,9 +13,9 @@ python imap_l3_data_processor.py --instrument glows --data-level l3b --start-dat
 '[{"type": "science", "files": ["imap_glows_l3a_hist_20100104_v002.cdf"]}]'
 elif [[ "$1" == "hi" ]]; then
 python imap_l3_data_processor.py --instrument hi --data-level l3 --start-date 20250415 \
---version v002 --descriptor "h90-sf-sp-hae-4deg-6mo" --dependency \
-'[{"type": "science", "files": ["imap_hi_l2_h90-sf-ram-hae-4deg-6mo_20250415_v002.cdf"]},
-{"type": "science", "files": ["imap_hi_l2_h90-sf-anti-hae-4deg-6mo_20250415_v002.cdf"]}]'
+--version v002 --descriptor "h90-ena-h-sf-sp-full-hae-4deg-6mo" --dependency \
+'[{"type": "science", "files": ["imap_hi_l2_h90-ena-h-sf-nsp-ram-hae-4deg-6mo_20250415_v001.cdf"]},
+{"type": "science", "files": ["imap_hi_l2_h90-ena-h-sf-nsp-anti-hae-4deg-6mo_20250415_v001.cdf"]}]'
 fi
 
 git restore imap_l3_processing/version.py
