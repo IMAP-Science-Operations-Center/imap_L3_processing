2b10b6d4170b42f6c79a321b6e7f7c677002dd45
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-05-13 08:57:09 -0400

    HARRISON L3 2569 - SWYN/KJON - CoDICE-Lo: Implemented fetch-dependencies (still missing geometric factors and efficiencies)

--- imap_l3_processing/codice/l3/lo/codice_lo_l3a_3d_distributions_dependencies.py
+++ imap_l3_processing/codice/l3/lo/codice_lo_l3a_3d_distributions_dependencies.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+import imap_data_access
+from imap_data_access.processing_input import ProcessingInputCollection, ScienceInput
+
+from imap_l3_processing.codice.l3.lo.codice_lo_l3a_direct_events_dependencies import DIRECT_EVENTS_DESCRIPTOR, \
+    SW_PRIORITY_DESCRIPTOR, NSW_PRIORITY_DESCRIPTOR
 from imap_l3_processing.codice.l3.lo.direct_events.science.mass_species_bin_lookup import MassSpeciesBinLookup
 from imap_l3_processing.codice.l3.lo.models import CodiceLoDirectEventData, CodiceLoL1aSWPriorityRates, \
     CodiceLoL1aNSWPriorityRates
+
+MASS_SPECIES_BIN_LOOKUP_DESCRIPTOR = "mass-species-bin-lookup"
 
 
 @dataclass
@@ -12,10 +19,40 @@ class CodiceLoL3a3dDistributionsDependencies:
     l1a_sw_data: CodiceLoL1aSWPriorityRates
     l1a_nsw_data: CodiceLoL1aNSWPriorityRates
     mass_species_bin_lookup: MassSpeciesBinLookup
-    
+
     @classmethod
-    def fetch_dependencies(cls, ):
-        pass
+    def fetch_dependencies(cls, processing_input_collection: ProcessingInputCollection):
+        l3a_direct_event_paths = []
+        l1a_sw_paths = []
+        l1a_nsw_paths = []
+        for input in processing_input_collection.get_science_inputs():
+            match input:
+                case ScienceInput(data_type="l3a", source="codice", descriptor=descriptor) \
+                    if descriptor == DIRECT_EVENTS_DESCRIPTOR:
+                    l3a_direct_event_paths.extend(input.filename_list)
+                case ScienceInput(data_type="l1a", source="codice", descriptor=descriptor) \
+                    if descriptor == SW_PRIORITY_DESCRIPTOR:
+                    l1a_sw_paths.extend(input.filename_list)
+                case ScienceInput(data_type="l1a", source="codice", descriptor=descriptor) \
+                    if descriptor == NSW_PRIORITY_DESCRIPTOR:
+                    l1a_nsw_paths.extend(input.filename_list)
+
+        [direct_events_path] = l3a_direct_event_paths
+        [sw_priority_rates_path] = l1a_sw_paths
+        [nsw_priority_rates_path] = l1a_nsw_paths
+        [mass_species_bin_path] = processing_input_collection.get_file_paths(source='codice',
+                                                                             descriptor=MASS_SPECIES_BIN_LOOKUP_DESCRIPTOR)
+
+        direct_events_downloaded_path = imap_data_access.download(direct_events_path)
+        sw_priority_rates_downloaded_path = imap_data_access.download(sw_priority_rates_path)
+        nsw_priority_rates_downloaded_path = imap_data_access.download(nsw_priority_rates_path)
+        mass_species_bin_downloaded_path = imap_data_access.download(mass_species_bin_path.name)
+
+        return cls.from_file_paths(l3a_file_path=direct_events_downloaded_path,
+                                   l1a_sw_file_path=sw_priority_rates_downloaded_path,
+                                   l1a_nsw_file_path=nsw_priority_rates_downloaded_path,
+                                   mass_species_bin_lut=mass_species_bin_downloaded_path,
+                                   )
 
     @classmethod
     def from_file_paths(cls, l3a_file_path: Path, l1a_sw_file_path: Path, l1a_nsw_file_path: Path,
--- tests/codice/l3/lo/test_codice_lo_l3a_3d_distributions_dependencies.py
+++ tests/codice/l3/lo/test_codice_lo_l3a_3d_distributions_dependencies.py
@@ -1,13 +1,55 @@
 import unittest
-from unittest.mock import patch, sentinel
+from unittest.mock import patch, sentinel, call
+
+from imap_data_access.processing_input import ProcessingInputCollection, ScienceInput, AncillaryInput
 
 from imap_l3_processing.codice.l3.lo.codice_lo_l3a_3d_distributions_dependencies import \
-    CodiceLoL3a3dDistributionsDependencies
+    CodiceLoL3a3dDistributionsDependencies, MASS_SPECIES_BIN_LOOKUP_DESCRIPTOR
+from imap_l3_processing.codice.l3.lo.codice_lo_l3a_direct_events_dependencies import SW_PRIORITY_DESCRIPTOR, \
+    NSW_PRIORITY_DESCRIPTOR
 
 
 class TestCodiceLoL3a3dDistributions(unittest.TestCase):
-    def test_fetch_dependencies(self):
-        pass
+    @patch(
+        "imap_l3_processing.codice.l3.lo.codice_lo_l3a_3d_distributions_dependencies.imap_data_access.download")
+    @patch(
+        "imap_l3_processing.codice.l3.lo.codice_lo_l3a_3d_distributions_dependencies.CodiceLoL3a3dDistributionsDependencies.from_file_paths")
+    def test_fetch_dependencies(self, mock_3d_distribution_deps_from_file_paths, mock_data_access_download):
+        l3a_direct_event_name = "imap_codice_l3a_lo-direct-events_20100105_v010.cdf"
+        l1a_sw_priority_name = f"imap_codice_l1a_{SW_PRIORITY_DESCRIPTOR}_20100105_v010.cdf"
+        l1a_nsw_priority_name = f"imap_codice_l1a_{NSW_PRIORITY_DESCRIPTOR}_20100105_v010.cdf"
+        mass_species_lut_name = f"imap_codice_{MASS_SPECIES_BIN_LOOKUP_DESCRIPTOR}_20100105_v010.cdf"
+
+        unused_science_input = ScienceInput(f"imap_codice_l2_lo-direct-events_20100105_v010.cdf")
+
+        processing_input_collection = ProcessingInputCollection(AncillaryInput(mass_species_lut_name),
+                                                                ScienceInput(l3a_direct_event_name),
+                                                                ScienceInput(l1a_sw_priority_name),
+                                                                ScienceInput(l1a_nsw_priority_name),
+                                                                unused_science_input)
+
+        mock_data_access_download.side_effect = [
+            sentinel.l3a_direct_event_downloaded_path,
+            sentinel.l1a_sw_priority_downloaded_path,
+            sentinel.l1a_nsw_priority_downloaded_path,
+            sentinel.mass_species_lut_downloaded_path,
+        ]
+
+        CodiceLoL3a3dDistributionsDependencies.fetch_dependencies(processing_input_collection)
+
+        mock_data_access_download.assert_has_calls([
+            call(l3a_direct_event_name),
+            call(l1a_sw_priority_name),
+            call(l1a_nsw_priority_name),
+            call(mass_species_lut_name),
+        ])
+
+        mock_3d_distribution_deps_from_file_paths.assert_called_once_with(
+            l3a_file_path=sentinel.l3a_direct_event_downloaded_path,
+            l1a_sw_file_path=sentinel.l1a_sw_priority_downloaded_path,
+            l1a_nsw_file_path=sentinel.l1a_nsw_priority_downloaded_path,
+            mass_species_bin_lut=sentinel.mass_species_lut_downloaded_path,
+        )
 
     @patch(
         'imap_l3_processing.codice.l3.lo.codice_lo_l3a_3d_distributions_dependencies.MassSpeciesBinLookup.read_from_csv')
