Harrison 2705 - PMCL/GLUN/SWYN/ETOB - Fix logic around converting obs_date from int to datetime to avoid overflow,
Propogate spice frame into all places we calculate survival prob

imap_l3_processing/maps/map_models.py
@@ -172,10 +172,10 @@ class SpectralIndexDependencies(metaclass=abc.ABCMeta):
 
 
 def _read_intensity_map_data_from_open_cdf(cdf: CDF) -> IntensityMapData:
+    masked_obs_date = read_variable_and_mask_fill_values(cdf["obs_date"])
     if np.issubdtype(cdf["obs_date"].dtype, np.number):
-        obs_date = read_variable_and_mask_fill_values(cdf["obs_date"]) / 1e9 * timedelta(seconds=1) + TT2000_EPOCH
-    else:
-        obs_date = read_variable_and_mask_fill_values(cdf["obs_date"])
+        obs_date = masked_obs_date.filled(0) / 1e9 * timedelta(seconds=1) + TT2000_EPOCH
+        masked_obs_date = np.ma.masked_array(data=obs_date, mask=masked_obs_date.mask)
 
     return IntensityMapData(
         epoch=cdf["epoch"][...],
@@ -187,7 +187,7 @@ def _read_intensity_map_data_from_open_cdf(cdf: CDF) -> IntensityMapData:
         latitude=read_numeric_variable(cdf["latitude"]),
         longitude=read_numeric_variable(cdf["longitude"]),
         exposure_factor=read_numeric_variable(cdf["exposure_factor"]),
-        obs_date=obs_date,
+        obs_date=masked_obs_date,
         obs_date_range=read_variable_and_mask_fill_values(cdf["obs_date_range"]),
         solid_angle=read_numeric_variable(cdf["solid_angle"]),
         ena_intensity=read_numeric_variable(cdf["ena_intensity"]),

scripts/hi/create_hi_full_spin_deps.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pathlib import Path
 
 import numpy as np
+from imap_processing.spice.geometry import SpiceFrame
 from spacepy.pycdf import CDF
 
 from imap_l3_processing.maps import survival_probability_processing
@@ -47,7 +48,7 @@ def create_hi_full_spin_deps(
 
     input_metadata = InputMetadata("hi", "l3", None, None, "v001", f"h90-ena-h-sf-sp-full-hae-4deg-6mo")
 
-    ram_data = survival_probability_processing.process_survival_probabilities(ramified_map_deps)
+    ram_data = survival_probability_processing.process_survival_probabilities(ramified_map_deps, SpiceFrame.ECLIPJ2000)
     ram_exposure_is_zero = (np.isnan(ram_data.intensity_map_data.ena_intensity)
                             | (ram_data.intensity_map_data.ena_intensity == 0))
 
@@ -65,7 +66,7 @@ def create_hi_full_spin_deps(
 
     shutil.move(ram_cdf_path, output_dir / f"{ram_logical_source}.cdf")
 
-    antiram_data = survival_probability_processing.process_survival_probabilities(antiramified_map_deps)
+    antiram_data = survival_probability_processing.process_survival_probabilities(antiramified_map_deps, SpiceFrame.ECLIPJ2000)
     antiram_exposure_is_zero = (np.isnan(antiram_data.intensity_map_data.ena_intensity)
                                 | (antiram_data.intensity_map_data.ena_intensity == 0))
 

tests/maps/test_map_models.py
@@ -1,5 +1,6 @@
 import dataclasses
 import os
+import sys
 import tempfile
 import unittest
 from datetime import datetime, timedelta
@@ -497,13 +498,16 @@ class TestMapModels(unittest.TestCase):
 
         map_data_shape = (1, 9, 90, 45)
         obs_date_datetime = np.full(map_data_shape, datetime.now())
+        obs_date_fillval = -sys.maxsize - 1
         test_cases = [
-            ("obs date is datetime", obs_date_datetime, obs_date_datetime),
-            ("obs date is int", np.full(map_data_shape, 1e9),
-             np.full(map_data_shape, TT2000_EPOCH) + timedelta(seconds=1)),
+            ("obs date is datetime", obs_date_datetime, obs_date_datetime, np.full(map_data_shape, False)),
+            ("obs date is int", np.full(map_data_shape, 1e9, dtype=int),
+             np.full(map_data_shape, TT2000_EPOCH) + timedelta(seconds=1), np.full(map_data_shape, False)),
+            ("obs date is all fill", np.full(map_data_shape, obs_date_fillval, dtype=int),
+             np.full(map_data_shape, TT2000_EPOCH), np.full(map_data_shape, True)),
         ]
 
-        for test_name, obs_date_in_cdf, expected_obs_date in test_cases:
+        for test_name, obs_date_in_cdf, expected_obs_date, expected_obs_date_mask in test_cases:
             with tempfile.TemporaryDirectory() as temp_dir:
                 pathname = os.path.join(temp_dir, "test_cdf")
                 with CDF(pathname, '') as cdf:
@@ -555,6 +559,9 @@ class TestMapModels(unittest.TestCase):
                     for var in cdf:
                         cdf[var].attrs['FILLVAL'] = 1000000
 
+                    cdf["obs_date"].attrs["FILLVAL"] = obs_date_fillval
+
+
                 for path in [pathname, Path(pathname)]:
                     with self.subTest(name=test_name, path=path):
                         result = RectangularIntensityMapData.read_from_path(path)
@@ -579,7 +586,8 @@ class TestMapModels(unittest.TestCase):
                         np.testing.assert_array_equal(ena_intensity_stat_unc, map_data.ena_intensity_stat_unc)
                         np.testing.assert_array_equal(ena_intensity_sys_err, map_data.ena_intensity_sys_err)
                         np.testing.assert_array_equal(exposure, map_data.exposure_factor)
-                        np.testing.assert_array_equal(expected_obs_date, map_data.obs_date)
+                        np.testing.assert_array_equal(expected_obs_date, map_data.obs_date.data)
+                        np.testing.assert_array_equal(expected_obs_date_mask, map_data.obs_date.mask)
                         np.testing.assert_array_equal(obs_date_range, map_data.obs_date_range)
                         np.testing.assert_array_equal(solid_angle, map_data.solid_angle)
 