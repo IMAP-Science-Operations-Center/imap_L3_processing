5ebd15447ae739192a8894ce9cfaf14d94e03928
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-09-17 12:37:57 -0400

    Harrison 2646 - HIT - KJON/PMCL Modified script for generating MAG CDF from CSV provided by instrument team, uploaded macropixel products generated using this MAG data

--- scripts/hit/create_mag_from_csv.py
+++ scripts/hit/create_mag_from_csv.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+import spiceypy
 from spacepy.pycdf import CDF
 
 
@@ -11,6 +12,11 @@ def create_mag(csv_path, l2_path, output_path, offset):
     csv_data = np.loadtxt(csv_path, delimiter=",", skiprows=1)
 
     mag_data = csv_data[offset:offset+epoch_len, -3:]
+    mag_sclk = csv_data[offset:offset+epoch_len, 6]
+    mag_epochs = [spiceypy.et2datetime(spiceypy.scs2e(-43, str(sclk))) for sclk in mag_sclk]
+    print(mag_sclk[0])
+    print(mag_epochs[:5])
+    print(epoch[:5])
 
     with CDF(output_path, create=True) as output_cdf:
         output_cdf['epoch'] = epoch
@@ -19,6 +25,11 @@ def create_mag(csv_path, l2_path, output_path, offset):
 
 
 if __name__ == '__main__':
+    spiceypy.furnsh("spice_kernels/imap_sclk_0000.tsc")
+    spiceypy.furnsh("spice_kernels/naif0012.tls")
     # USAGE: python create_mag_from_csv.py <path to csv> <path to l2 cdf> <output path> <offset>
     csv_path, l2_path, output_path, offset = sys.argv[1:]
     create_mag(csv_path, l2_path, output_path, int(offset))
+
+    # python scripts/hit/create_mag_from_csv.py instrument_team_data/hit/hit_l1a_sample2_fakeMAG.csv tests/test_data/hit/imap_hit_l2_macropixel-intensity_20100106_v001.cdf tests/test_data/hit/mag_data_for_hit_20100106 900
+    # python scripts/hit/create_mag_from_csv.py instrument_team_data/hit/hit_l1a_sample2_fakeMAG.csv tests/test_data/hit/imap_hit_l2_macropixel-intensity_20100107_v001.cdf tests/test_data/hit/mag_data_for_hit_20100107 2340
