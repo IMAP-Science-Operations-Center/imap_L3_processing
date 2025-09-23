18fdbc0b6b36e8f61aa2bc5e4fe920f0e5c456b2
Petty Dev Tigris <akaumeheiwa@menloinnovations.com>
2025-09-23 09:57:24 -0400

    Harrison 2744 - SWYN/EYEA - Updated create_l2_glows_cdf_from_json.py to output CDFs in the official L2 format

--- scripts/glows/create_l2_glows_cdf_from_json.py
+++ scripts/glows/create_l2_glows_cdf_from_json.py
@@ -1,76 +1,106 @@
 import json
-import re
+import math
+import shutil
 import traceback
 from datetime import datetime, timedelta
 from pathlib import Path
 
 import numpy as np
+from imap_data_access import ScienceFilePath
 from spacepy.pycdf import CDF
 
-
-def set_up_required_data_values(cdf, l2_data):
-    for key in ['identifier', 'filter_temperature_average', 'filter_temperature_std_dev',
-                'hv_voltage_average', 'hv_voltage_std_dev', 'spin_period_average', 'spin_period_std_dev',
-                'spin_period_ground_average', 'spin_period_ground_std_dev', 'pulse_length_average',
-                'pulse_length_std_dev',
-                'position_angle_offset_average', 'position_angle_offset_std_dev']:
-        cdf[key] = [l2_data[key]]
-
-    for key1 in ['spin_axis_orientation_average', 'spin_axis_orientation_std_dev']:
-        for key2 in ['lon', 'lat']:
-            cdf[f"{key1}_{key2}"] = [l2_data[key1][key2]]
-
-    for key1 in ['spacecraft_location_average', 'spacecraft_location_std_dev',
-                 'spacecraft_velocity_average', 'spacecraft_velocity_std_dev']:
-        for key2 in ['x', 'y', 'z']:
-            cdf[f"{key1}_{key2}"] = [l2_data[key1][key2]]
+from imap_l3_processing.constants import TTJ2000_EPOCH
+from tests.test_helpers import get_test_data_path
 
 
-def create_l2_glows_cdf_from_json(json_file_path: str, output_filename: str):
-    with CDF(output_filename, '') as cdf:
-        cdf.col_major(True)
-        with open(json_file_path) as f:
-            data = json.load(f)
-            set_up_required_data_values(cdf, data)
-            light_curve = data["daily_lightcurve"]
+def fill_official_l2_cdf_with_json_values(output_folder: Path, input_path: Path) -> Path:
+    official_l2_path = get_test_data_path("glows/imap_glows_l2_hist_20130908-repoint01000_v001.cdf")
 
-            start_of_epoch_window = datetime.fromisoformat(data["start_time"])
-            end_of_epoch_window = datetime.fromisoformat(data["end_time"])
-            epoch_window = end_of_epoch_window - start_of_epoch_window
-            epoch = start_of_epoch_window + epoch_window / 2
+    with open(input_path) as f:
+        instrument_data = json.load(f)
 
-            cdf["start_time"] = np.array([start_of_epoch_window])
-            cdf["end_time"] = np.array([end_of_epoch_window])
-            cdf["epoch"] = np.array([epoch])
-            cdf["epoch_delta"] = np.array([int(epoch_window.total_seconds() * 1e9 / 2)])
-            int_flags = [int(f, 16) for f in light_curve["histogram_flag_array"]]
-            flag_bits = np.unpackbits(np.array([int_flags], dtype=np.uint8), axis=0, bitorder='little', count=4)
-            cdf["histogram_flag_array"] = [flag_bits]
+        start_of_epoch_window = datetime.fromisoformat(instrument_data["start_time"])
+        end_of_epoch_window = datetime.fromisoformat(instrument_data["end_time"])
+        epoch_window = end_of_epoch_window - start_of_epoch_window
+        epoch = start_of_epoch_window + epoch_window / 2
 
-            cdf.new("number_of_bins", light_curve["number_of_bins"], recVary=False)
+        repoint_id = 149 + math.floor((epoch - datetime(2010, 1, 1)) / timedelta(days=1))
+
+        new_name = ScienceFilePath.generate_from_inputs(
+            instrument="glows",
+            data_level="l2",
+            descriptor="hist",
+            start_time=start_of_epoch_window.strftime("%Y%m%d"),
+            version="v003",
+            extension="cdf",
+            repointing=repoint_id
+        ).filename
+
+        new_file_path = output_folder / new_name
+        new_file_path.unlink(missing_ok=True)
+
+        with CDF(str(new_file_path), masterpath=str(official_l2_path)) as cdf:
+            print(f"Writing to file {new_name}")
+            cdf["epoch"][0] = epoch
+            cdf['start_time'][0] = (start_of_epoch_window - TTJ2000_EPOCH).total_seconds() * 1e9
+            cdf['end_time'][0] = (end_of_epoch_window - TTJ2000_EPOCH).total_seconds() * 1e9
 
             lightcurve_vars = [
-                "spin_angle", "photon_flux", "exposure_times", "flux_uncertainties",
-                "raw_histogram", "ecliptic_lon", "ecliptic_lat"
+                "spin_angle",
+                "photon_flux",
+                "exposure_times",
+                "flux_uncertainties",
+                "ecliptic_lon",
+                "ecliptic_lat",
             ]
             for var in lightcurve_vars:
-                cdf[var] = np.reshape(light_curve[var], (1, -1))
+                cdf[var] = np.array(instrument_data["daily_lightcurve"][var])[np.newaxis, :]
 
-            cdf.attrs["flight_software_version"] = data["header"]["flight_software_version"]
-            cdf.attrs["pkts_file_name"] = data["header"]["pkts_file_name"]
-            cdf.attrs["ancillary_data_files"] = data["header"]["ancillary_data_files"]
+            cdf["raw_histograms"] = np.array(instrument_data["daily_lightcurve"]["raw_histogram"])[np.newaxis, :]
+            cdf["histogram_flag_array"] = np.array(
+                [int(f, 16) for f in instrument_data["daily_lightcurve"]["histogram_flag_array"]])[np.newaxis, :]
+
+            single_value_vars = [
+                "filter_temperature_average",
+                "filter_temperature_std_dev",
+                "hv_voltage_average",
+                "hv_voltage_std_dev",
+                "spin_period_average",
+                "spin_period_std_dev",
+                "pulse_length_average",
+                "pulse_length_std_dev",
+                "spin_period_ground_average",
+                "spin_period_ground_std_dev",
+                "position_angle_offset_average",
+                "position_angle_offset_std_dev",
+                "identifier"
+            ]
+            for var in single_value_vars:
+                cdf[var][0] = instrument_data[var]
+
+            vector_vars = [
+                "spacecraft_location_average",
+                "spacecraft_location_std_dev",
+                "spacecraft_velocity_average",
+                "spacecraft_velocity_std_dev",
+                "spin_axis_orientation_average",
+                "spin_axis_orientation_std_dev"
+            ]
+            for var in vector_vars:
+                cdf[var][0] = np.array(list(instrument_data[var].values()))
+
+            cdf["bad_time_flag_occurrences"][0] = list(instrument_data["bad_time_flag_occurences"].values()) # for old instrument team data
+            cdf["number_of_good_l1b_inputs"][0] = instrument_data["header"]["number_of_l1b_files_used"]
+            cdf["total_l1b_inputs"][0] = instrument_data["header"]["number_of_all_l1b_files"]
 
 
 if __name__ == "__main__":
-    json_directory = Path(r'C:\Users\Harrison\Downloads\l2_data_2')
-    output_directory = Path(r'C:\Users\Harrison\Downloads\p3')
+    json_directory = Path(r'C:\Users\Petty\Downloads\data_products_cbk_implementation_2024-12-31_1year\data_l2_histograms')
+    output_directory = Path(r'C:\Users\Petty\Downloads\l2_cdfs')
+    shutil.rmtree(output_directory, ignore_errors=True)
+    output_directory.mkdir(parents=True, exist_ok=True)
     for file_path in json_directory.iterdir():
-        output = re.search(r"imap_glows_l2_(\d{8}).*_orbX_modX_p_v00.json", file_path.name)
-        output_file_path_date = output.group(1)
-        d = datetime.strptime(output_file_path_date, "%Y%m%d")
-        repoint_id = 150 + round((d - datetime(2010, 1, 2)) / timedelta(days=1))
-        cdf_file_path = f"{output_directory}/imap_glows_l2_hist_{output_file_path_date}-repoint{repoint_id:05}_v001.cdf"
         try:
-            create_l2_glows_cdf_from_json(file_path, cdf_file_path)
+            fill_official_l2_cdf_with_json_values(output_directory, file_path)
         except Exception as e:
             traceback.print_exc()
