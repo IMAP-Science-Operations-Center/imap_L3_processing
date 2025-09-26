fbe37bdf219d6b0057cf8447c83b2986a83f94e8
Petty Dev Tigris <akaumeheiwa@menloinnovations.com>
2025-09-26 14:23:19 -0400

    Harrison 2788 - SWYN/EYEA - SWAPI: run_local points to new ancillary files

--- run_local.py
+++ run_local.py
@@ -1044,13 +1044,13 @@ if __name__ == "__main__":
     if "swapi" in sys.argv:
         if "l3a" in sys.argv:
             paths = create_swapi_l3a_cdf(
-                "tests/test_data/swapi/imap_swapi_proton-density-temperature-lut_20240905_v000.dat",
-                "tests/test_data/swapi/imap_swapi_alpha-density-temperature-lut_20240920_v000.dat",
-                "tests/test_data/swapi/imap_swapi_clock-angle-and-flow-deflection-lut_20240918_v000.dat",
-                "tests/test_data/swapi/imap_swapi_energy-gf-pui-lut_20100101_v001.csv",
-                "tests/test_data/swapi/imap_swapi_instrument-response-lut_20241023_v000.zip",
-                "tests/test_data/swapi/imap_swapi_l2_density-of-neutral-helium-lut-text-not-cdf_20241023_v002.cdf",
-                "tests/test_data/swapi/imap_swapi_efficiency-lut_20241020_v000.dat",
+                str(get_test_data_path("swapi/imap_swapi_proton-density-temperature-lut_20240905_v001.dat")),
+                str(get_test_data_path("swapi/imap_swapi_alpha-density-temperature-lut_20240920_v000.dat")),
+                str(get_test_data_path("swapi/imap_swapi_clock-angle-and-flow-deflection-lut_20240918_v001.dat")),
+                str(get_test_data_path("swapi/imap_swapi_energy-gf-pui-lut_20100101_v001.csv")),
+                str(get_test_data_path("swapi/imap_swapi_instrument-response-lut_20241023_v000.zip")),
+                str(get_test_data_path("swapi/imap_swapi_l2_density-of-neutral-helium-lut-text-not-cdf_20241023_v002.cdf")),
+                str(get_test_data_path("swapi/imap_swapi_efficiency-lut_20241020_v000.dat")),
                 str(get_test_data_path("swapi/imap_swapi_l2_50-sweeps_20250606_v003.cdf")),
             )
             print(paths)
