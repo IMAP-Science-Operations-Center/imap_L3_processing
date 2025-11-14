8177a97a189e3b256941d23080b982f8411f6d57
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-11-14 14:56:34 -0500

    Harrison L3 - EYEA/PSCH/GLUN - Add script to download parents of files in folder

---
+++ scripts/batch_download_parents.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from pathlib import Path
+
+
+def download_parents(path: Path):
+    initial_cwd = os.getcwd()
+    os.chdir(path)
+    import imap_data_access
+    from spacepy.pycdf import CDF
+
+    try:
+        files_to_download = set()
+
+        for file in path.rglob("*_l3_*.cdf"):
+            print(f"opening {path / file}")
+            with CDF(str(path / file)) as cdf:
+                for parent in cdf.attrs['Parents']:
+                    files_to_download.add(parent)
+
+        print(f"Downloading {len(files_to_download)} files")
+
+        for file in files_to_download:
+            print(imap_data_access.download(file))
+
+    except Exception as e:
+        print(e)
+        os.chdir(initial_cwd)
+
+
+if __name__ == '__main__':
+    download_parents(Path(sys.argv[1]))
