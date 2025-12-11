99930fae66ce4fe0f53c23fde7a63c74ace4cb47
Menlo Developer <harrison@menloinnovations.com>
2025-12-11 09:24:29 -0500

    Harrison L3 - EYEA/SWYN - Ultra: Break up all-maps job into multiple, smaller jobs

--- tests/test_imap_l3_data_processor.py
+++ tests/test_imap_l3_data_processor.py
@@ -7,11 +7,12 @@ from imap_data_access.processing_input import ScienceInput, ProcessingInputColle
     AncillaryInput, SPICEInput
 
 from imap_l3_data_processor import imap_l3_processor
-from imap_l3_processing.hi.l3.hi_l3_initializer import HI_SP_MAP_DESCRIPTORS
+from imap_l3_processing.hi.l3.hi_l3_initializer import HI_SP_MAP_DESCRIPTORS, HI_COMBINED_DESCRIPTORS
 from imap_l3_processing.lo.l3.lo_initializer import LO_SP_MAP_DESCRIPTORS
 from imap_l3_processing.maps.map_initializer import PossibleMapToProduce
 from imap_l3_processing.models import InputMetadata
-from imap_l3_processing.ultra.l3.ultra_initializer import ULTRA_SP_MAP_DESCRIPTORS
+from imap_l3_processing.ultra.l3.ultra_initializer import ULTRA_45_DESCRIPTORS, ULTRA_90_DESCRIPTORS, \
+    ULTRA_COMBINED_SP_DESCRIPTORS, ULTRA_COMBINED_NSP_DESCRIPTORS
 
 
 class TestImapL3DataProcessor(TestCase):
@@ -38,9 +39,9 @@ class TestImapL3DataProcessor(TestCase):
             ("glows", "l3b", "ion-rate-profile", mock_glows),
             ("swe", "l3", "sci", mock_swe),
             ("hit", "l3", "macropixel", mock_hit),
-            ("hi", "l3", "hi-descriptor", mock_hi),
-            ("ultra", "l3", "ultra-descriptor", mock_ultra),
-            ("lo", "l3", "lo-descriptor", mock_lo),
+            ("hi", "l3", "h45-ena-h-hf-sp-full-hae-2deg-6mo", mock_hi),
+            ("ultra", "l3", "u45-spx-h-hf-sp-full-hae-2deg-6mo", mock_ultra),
+            ("lo", "l3", "l090-spx-h-hf-sp-ram-hae-2deg-6mo", mock_lo),
             ("codice", "l3a", "hi-direct-events", mock_codice_hi),
             ("codice", "l3b", "hi-pitch-angle", mock_codice_hi),
             ("codice", "l3a", "lo-direct-events", mock_codice_lo),
@@ -102,20 +103,27 @@ class TestImapL3DataProcessor(TestCase):
                                                   mock_hi_processor_class, mock_hi_initializer_class,
                                                   mock_ultra_processor_class, mock_ultra_initializer_class,
                                                   ):
-
         test_cases = [
-            ("hi", mock_hi_initializer_class, mock_hi_processor_class, HI_SP_MAP_DESCRIPTORS),
-            ("lo", mock_lo_initializer_class, mock_lo_processor_class, LO_SP_MAP_DESCRIPTORS),
-            ("ultra", mock_ultra_initializer_class, mock_ultra_processor_class, ULTRA_SP_MAP_DESCRIPTORS),
+            ("hi", "sp-maps", mock_hi_initializer_class, mock_hi_processor_class, HI_SP_MAP_DESCRIPTORS),
+            ("hi", "hic-maps", mock_hi_initializer_class, mock_hi_processor_class, HI_COMBINED_DESCRIPTORS),
+            ("lo", "all-maps", mock_lo_initializer_class, mock_lo_processor_class, LO_SP_MAP_DESCRIPTORS),
+            ("ultra", "u45-maps", mock_ultra_initializer_class, mock_ultra_processor_class, ULTRA_45_DESCRIPTORS),
+            ("ultra", "u90-maps", mock_ultra_initializer_class, mock_ultra_processor_class, ULTRA_90_DESCRIPTORS),
+            (
+                "ultra", "ulc-sp-maps", mock_ultra_initializer_class, mock_ultra_processor_class,
+                ULTRA_COMBINED_SP_DESCRIPTORS),
+            ("ultra", "ulc-nsp-maps", mock_ultra_initializer_class, mock_ultra_processor_class,
+             ULTRA_COMBINED_NSP_DESCRIPTORS),
         ]
 
-        for instrument, mock_initializer_class, mock_processor_class, expected_descriptors in test_cases:
+        for instrument, descriptor, mock_initializer_class, mock_processor_class, expected_descriptors in test_cases:
             mock_upload.reset_mock()
             mock_argparse.reset_mock()
+            mock_initializer_class.reset_mock()
+            mock_processor_class.reset_mock()
 
             with self.subTest(instrument=instrument):
                 data_level = "l3"
-                descriptor = "all-maps"
                 mock_argument_parser = mock_argparse.ArgumentParser.return_value
                 mock_argument_parser.parse_args.return_value.instrument = instrument
                 mock_argument_parser.parse_args.return_value.data_level = data_level
