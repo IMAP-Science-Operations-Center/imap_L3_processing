bef7b9599843405d6eba10b703cba0a1da145f97
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-12-16 09:21:54 -0500

    Harrison 2767 - KJON/GSTU - Ultra: fix dimensions for solid angle in L2 xarray

--- imap_l3_processing/maps/map_models.py
+++ imap_l3_processing/maps/map_models.py
@@ -231,9 +231,9 @@ def _read_intensity_map_data_from_xarray(dataset: xarray.Dataset) -> IntensityMa
         latitude=_replace_fill_values_in_xarray(dataset, "latitude"),
         longitude=_replace_fill_values_in_xarray(dataset, "longitude"),
         exposure_factor=_replace_fill_values_in_xarray(dataset, "exposure_factor"),
-        obs_date=np.squeeze(_replace_fill_values_in_xarray(dataset, "obs_date")),
+        obs_date=_replace_fill_values_in_xarray(dataset, "obs_date"),
         obs_date_range=_replace_fill_values_in_xarray(dataset, "obs_date_range"),
-        solid_angle=_replace_fill_values_in_xarray(dataset, "solid_angle"),
+        solid_angle=np.squeeze(_replace_fill_values_in_xarray(dataset, "solid_angle")),
         ena_intensity=_replace_fill_values_in_xarray(dataset, "ena_intensity"),
         ena_intensity_stat_uncert=_replace_fill_values_in_xarray(dataset, "ena_intensity_stat_uncert"),
         ena_intensity_sys_err=_replace_fill_values_in_xarray(dataset, "ena_intensity_sys_err")
