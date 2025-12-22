b0041013227eb2ef4d9e2931bff71f5348be34b8
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-12-22 12:40:30 -0500

    Harrison L3 2862 - KJON/PMCL - Revert accidentally-committed change to MAG variable name

--- imap_l3_processing/utils.py
+++ imap_l3_processing/utils.py
@@ -211,7 +211,7 @@ def read_l1d_mag_data(cdf_path: Union[str, Path]) -> MagL1dData:
     with CDF(str(cdf_path)) as cdf:
         return MagL1dData(
             epoch=cdf['epoch'][...],
-            mag_data=read_numeric_variable(cdf["vectors"])[:, :3])
+            mag_data=read_numeric_variable(cdf["b_dsrf"])[:, :3])
 
 
 L1CPointingSet = TypeVar("L1CPointingSet", bound=Union[InputRectangularPointingSet, UltraL1CPSet])
