46ef889f1783becc93c0f2cbc3bf19838a2a2cbc
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2026-01-12 15:43:58 -0500

    Harrison L3 - SWE & SWAPI - PMCL/KJON - Updated script to run SWE in parallel

--- scripts/swe/generate_sci_using_swapi_with_predicted_ephemeris.py
+++ scripts/swe/generate_sci_using_swapi_with_predicted_ephemeris.py
@@ -1,10 +1,10 @@
-import json
 import subprocess
 import sys
 from datetime import datetime, timedelta
 
 import imap_data_access
-from imap_data_access import DependencyFilePath
+from imap_data_access import DependencyFilePath, ProcessingInputCollection
+from imap_data_access.processing_input import generate_imap_input
 
 from imap_l3_processing.utils import get_spice_kernels_file_names, SpiceKernelTypes
 
@@ -31,7 +31,7 @@ def generate_swe_for_given_day(day: datetime):
                                                end_date=day_as_string, version='latest', descriptor='norm-dsrf')[0][
         'file_path']
 
-    swapi_l3a_file_name = f'imap_swapi_l3a_proton-sw_{day_as_string}_v002.cdf'
+    swapi_l3a_file_name = f'imap_swapi_l3a_proton-sw_{day_as_string}_v003.cdf'
 
     swe_l1b_file_name = imap_data_access.query(instrument='swe', data_level='l1b', start_date=day_as_string,
                                                end_date=day_as_string, version='latest', descriptor='sci')[0][
@@ -39,67 +39,33 @@ def generate_swe_for_given_day(day: datetime):
 
     swe_ancillary_file_name = "imap_swe_config_20251119_v002.json"
 
-    swe_data_as_json = []
+    input_files = [
+        *spice_kernel_files,
+        l2_file_name,
+        swe_l1b_file_name,
+        mag_l1d_file_name,
+        swapi_l3a_file_name,
+        swe_ancillary_file_name
+    ]
 
-    swe_data_as_json.append(
-        {
-            "type": "spice",
-            "files": spice_kernel_files
-        }
-    )
-    swe_data_as_json.append(
-        {
-            "type": "science",
-            "files": [l2_file_name]
-        }
-    )
-    swe_data_as_json.append(
-        {
-            "type": "science",
-            "files": [swe_l1b_file_name]
-        }
-    )
-    swe_data_as_json.append(
-        {
-            "type": "science",
-            "files": [mag_l1d_file_name]
-        }
-    )
-    swe_data_as_json.append(
-        {
-            "type": "science",
-            "files": [swapi_l3a_file_name]
-        }
-    )
-    swe_data_as_json.append(
-        {
-            "type": "ancillary",
-            "files": [swe_ancillary_file_name]
-        }
-    )
-    swe_data_as_json.append(
-        {
-            "type": "spice",
-            "files": spice_kernel_files
-        }
-    )
+    input_collection = ProcessingInputCollection(*[generate_imap_input(i) for i in input_files])
+    input_collection.download_all_files()
 
     dependency_file_name = f'imap_swe_l3_sci_{day_as_string}_v002.json'
     output_filepath = DependencyFilePath(dependency_file_name)
 
     fullpath = output_filepath.construct_path()
     fullpath.parent.mkdir(parents=True, exist_ok=True)
-    with open(fullpath, mode='wt') as output_file:
-        json.dump(swe_data_as_json, output_file, indent=4)
+
+    fullpath.write_text(input_collection.serialize())
 
     args_string = (f"imap_l3_data_processor.py "
                    f"--instrument swe --data-level l3 --descriptor sci "
                    f"--start-date {day_as_string} --version v004 "
                    f"--dependency {dependency_file_name}")
 
-    pid = subprocess.Popen([sys.executable, *args_string.split(" ")])
+    return subprocess.Popen([sys.executable, *args_string.split(" ")])
 
-    return pid
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -111,7 +77,6 @@ if __name__ == '__main__':
             datetime(2025, 12, 17),
             datetime(2025, 12, 18),
         ]
-
     pids = [generate_swe_for_given_day(date) for date in dates]
 
     for pid in pids:
