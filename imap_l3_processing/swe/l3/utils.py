aeb141fad53d1aefe6613379f66a17f075432be9
mecuesta <mecuesta@princeton.edu>
2026-04-09 11:53:53 -0400

    Reverting back to original file

--- imap_l3_processing/swe/l3/utils.py
+++ imap_l3_processing/swe/l3/utils.py
@@ -57,11 +57,7 @@ def read_l3a_swapi_proton_data(swapi_l3a_data: Path) -> SwapiL3aProtonData:
         proton_sw_speed = read_numeric_variable(cdf["proton_sw_speed"])
         proton_sw_clock_angle = read_numeric_variable(cdf["proton_sw_clock_angle"])
         proton_sw_deflection_angle = read_numeric_variable(cdf["proton_sw_deflection_angle"])
-        ### MEC: insert try-except to catch swp_flags issue
-        try:
-            swp_flags = read_numeric_variable(cdf["swp_flags"])
-        except:
-            swp_flags = np.zeros(len(epoch))
+        swp_flags = read_numeric_variable(cdf["swp_flags"])
 
     return SwapiL3aProtonData(epoch=epoch,
                               epoch_delta=epoch_delta,
