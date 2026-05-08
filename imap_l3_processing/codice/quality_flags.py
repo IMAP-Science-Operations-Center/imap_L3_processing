8f9e087489bb841429768052eea737c87a946252
Harrison <pleasant@menloinnovations.com>
2026-05-08 07:34:42 -0400

    Harrison 3156 - LFAR - preliminary mag flag value now matches same bit as SWE

--- imap_l3_processing/codice/quality_flags.py
+++ imap_l3_processing/codice/quality_flags.py
@@ -3,4 +3,4 @@ from imap_processing.quality_flags import FlagNameMixin, CommonFlags
 
 class CodiceL3Flags(FlagNameMixin):
     NONE = CommonFlags.NONE
-    PRELIMINARY_MAG = 2 ** 2
+    PRELIMINARY_MAG = 2 ** 8
--- imap_l3_processing/hit/quality_flags.py
+++ imap_l3_processing/hit/quality_flags.py
@@ -3,4 +3,4 @@ from imap_processing.quality_flags import FlagNameMixin, CommonFlags
 
 class HitL3Flags(FlagNameMixin):
     NONE = CommonFlags.NONE
-    PRELIMINARY_MAG = 2 ** 2
+    PRELIMINARY_MAG = 2 ** 8
