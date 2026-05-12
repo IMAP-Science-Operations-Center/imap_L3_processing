4b6dd1e7b4bb3b2e8cd76d014b3b7be0ae28294b
Harrison <pleasant@menloinnovations.com>
2026-05-12 15:57:29 -0400

    HARRISON 3156 - #13 - ETOB/EYEA - Updated quality flag bit location for Preliminary Mag

--- imap_l3_processing/codice/quality_flags.py
+++ imap_l3_processing/codice/quality_flags.py
@@ -3,4 +3,4 @@ from imap_processing.quality_flags import FlagNameMixin, CommonFlags
 
 class CodiceL3Flags(FlagNameMixin):
     NONE = CommonFlags.NONE
-    PRELIMINARY_MAG = 2 ** 8
+    PRELIMINARY_MAG = 2 ** 2
--- imap_l3_processing/hit/quality_flags.py
+++ imap_l3_processing/hit/quality_flags.py
@@ -3,4 +3,4 @@ from imap_processing.quality_flags import FlagNameMixin, CommonFlags
 
 class HitL3Flags(FlagNameMixin):
     NONE = CommonFlags.NONE
-    PRELIMINARY_MAG = 2 ** 8
+    PRELIMINARY_MAG = 2 ** 2
