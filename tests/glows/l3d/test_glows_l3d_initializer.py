69e234efec17769de116248a237c38d1fcd3a6d5
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2026-08-06 17:00:56 -0400

    Harrison 3412 - AFAH/NHUT - WIP Writing test for forcing reprocessing l3d

--- tests/glows/l3d/test_glows_l3d_initializer.py
+++ tests/glows/l3d/test_glows_l3d_initializer.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, call, Mock
+from unittest.mock import patch, call, Mock, sentinel
 
 from imap_data_access.file_validation import Version
 
@@ -192,7 +192,7 @@ class TestGlowsL3DInitializer(unittest.TestCase):
     @patch('imap_l3_processing.glows.l3d.glows_l3d_initializer.read_cdf_parents')
     @patch('imap_l3_processing.glows.l3d.glows_l3d_initializer.imap_data_access.query')
     @patch('imap_l3_processing.glows.l3d.glows_l3d_initializer.GlowsL3DDependencies.fetch_dependencies')
-    def test_l3d_initializer_should_not_process(self, mock_fetch_l3d_deps, mock_query,
+    def test_l3d_initializer_should_not_process_if_same_major_version(self, mock_fetch_l3d_deps, mock_query,
                                                 mock_read_cdf_parents, mock_read_pipeline_settings, _):
         l3bs = [
             'imap_glows_l3b_ion-rate-profile_20200101-cr00000_v000.cdf',
@@ -255,6 +255,74 @@ class TestGlowsL3DInitializer(unittest.TestCase):
         actual_l3d_deps = GlowsL3DInitializer.should_process_l3d(external_dependencies, l3bs, l3cs, None)
         mock_fetch_l3d_deps.assert_not_called()
         self.assertIsNone(actual_l3d_deps)
+
+    @patch('imap_l3_processing.glows.l3d.glows_l3d_initializer.read_cdf_parents')
+    @patch('imap_l3_processing.glows.l3d.glows_l3d_initializer.imap_data_access.download')
+    @patch('imap_l3_processing.glows.l3d.glows_l3d_initializer.imap_data_access.query')
+    @patch('imap_l3_processing.glows.l3d.glows_l3d_initializer.read_pipeline_settings')
+    @patch('imap_l3_processing.glows.l3d.glows_l3d_initializer.GlowsL3DDependencies.fetch_dependencies')
+    def test_l3d_initializer_forces_reprocess_if_reprocess_ancillary_specifies_l3e(self, mock_fetch_dependencies, mock_read_pipeline_settings, mock_query, mock_download, mock_read_cdf_parents):
+        most_recent_l3d = 'imap_glows_l3d_solar-hist_19470301-cr00002_v001.0002.cdf'
+
+        mock_fetch_dependencies.return_value = sentinel.l3d_deps
+        mock_query.side_effect = self._create_ancillary_query_results([most_recent_l3d])
+        mock_read_pipeline_settings.return_value = {"start_cr": 1}
+
+        l3bs = [
+            'imap_glows_l3b_ion-rate-profile_20200101-cr00000_v000.cdf',
+            'imap_glows_l3b_ion-rate-profile_20200101-cr00001_v000.cdf',
+        ]
+        l3cs = [
+            'imap_glows_l3c_sw-profile_20200101-cr00000_v000.cdf',
+            'imap_glows_l3c_sw-profile_20200101-cr00001_v000.cdf',
+        ]
+
+        external_dependencies = Mock()
+
+        mock_read_cdf_parents.return_value = {
+            'imap_glows_l3b_ion-rate-profile_20200101-cr00000_v000.cdf',
+            'imap_glows_l3b_ion-rate-profile_20200201-cr00001_v000.cdf',
+            'imap_glows_l3c_sw-profile_20200101-cr00000_v000.cdf',
+            'imap_glows_l3c_sw-profile_20200201-cr00001_v000.cdf',
+            'imap_glows_plasma-speed-2026d_19470301_v001.dat',
+            'imap_glows_proton-density-2026d_19470301_v000.dat',
+            'imap_glows_uv-anisotropy-2026d_19470301_v000.dat',
+            'imap_glows_photoion-2026d_19470301_v000.dat',
+            'imap_glows_lya-2026d_19470301_v000.dat',
+            'imap_glows_electron-density-2026d_19470301_v000.dat'
+        }
+
+        version, l3d_deps, old_l3d = GlowsL3DInitializer.should_process_l3d(external_dependencies, l3bs, l3cs, 1)
+
+        self.assertEqual(Version(1, 3), version)
+        self.assertIs(sentinel.l3d_deps, l3d_deps)
+        self.assertEqual(most_recent_l3d, old_l3d)
+
+        [fetch_dependencies_call] = mock_fetch_dependencies.call_args_list
+
+        [actual_l3d_inputs, actual_external_deps] = fetch_dependencies_call.args
+
+        pipeline_l3d_input_paths = actual_l3d_inputs.get_file_paths(source="glows")
+        pipeline_l3d_input_filenames = [p.name for p in pipeline_l3d_input_paths]
+        self.assertEqual([
+            'imap_glows_l3b_ion-rate-profile_20200101-cr00001_v000.cdf',
+            'imap_glows_l3b_ion-rate-profile_20200201-cr00002_v001.cdf',
+            'imap_glows_l3b_ion-rate-profile_20200301-cr00003_v000.cdf',
+            'imap_glows_l3c_sw-profile_20200101-cr00001_v000.cdf',
+            'imap_glows_l3c_sw-profile_20200201-cr00002_v001.cdf',
+            'imap_glows_l3c_sw-profile_20200301-cr00003_v000.cdf',
+            'imap_glows_plasma-speed-2026d_19470301_v001.dat',
+            'imap_glows_proton-density-2026d_19470301_v000.dat',
+            'imap_glows_uv-anisotropy-2026d_19470301_v000.dat',
+            'imap_glows_photoion-2026d_19470301_v000.dat',
+            'imap_glows_lya-2026d_19470301_v000.dat',
+            'imap_glows_electron-density-2026d_19470301_v000.dat',
+            'imap_glows_pipeline-settings-l3bcde_19470301_v000.json'
+        ], pipeline_l3d_input_filenames)
+
+        self.assertEqual(external_dependencies, actual_external_deps)
+
+
 
     def _create_ancillary_query_results(self, l3d_query_result):
         return [
