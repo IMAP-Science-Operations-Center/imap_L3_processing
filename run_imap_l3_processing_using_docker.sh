33874b0322812768fae823c928a952acdbdae242
Leo Werneck <leo.werneck@princeton.edu>
2026-09-23 16:14:18 -0400

    run_imap_l3_processing_using_docker.sh: reverted previous deletion commit (wrong file), then renamed it to existing file

--- run_imap_l3_processing_using_docker.sh
+++ run_imap_l3_processing_using_docker.sh
@@ -1,9 +1,27 @@
-source scripts/update_version.sh
+#!/bin/bash
 
-docker run --rm \
---mount type=bind,src="$(pwd)/temp_cdf_data",dst="/temp_cdf_data" \
---mount type=bind,src="$(pwd)/data",dst="/data" \
--e IMAP_API_KEY=$IMAP_API_KEY \
-$(docker build -q .) $@
+set -e
 
-git restore imap_l3_processing/version.py
+if [[ -z "$IMAP_API_KEY" ]]; then
+    echo "Warning: IMAP_API_KEY not set; you might get errors."
+fi
+
+if [[ -z "$IMAP_DATA_DIR" ]]; then
+    IMAP_DATA_DIR="$PWD/data"
+    mkdir -p "$IMAP_DATA_DIR"
+    echo "Warning: IMAP_DATA_DIR not set; using default directory ('$IMAP_DATA_DIR')"
+fi
+
+if ! docker image inspect imap-l3 >/dev/null 2>&1; then
+    echo "Docker image 'imap-l3' not found; building it..."
+    docker build --platform=linux/amd64 -t imap-l3 .
+    echo "Done!"
+fi
+
+echo "Running 'imap-l3' Docker image..."
+docker run --platform=linux/amd64 --rm \
+    -e IMAP_API_KEY \
+    -e IMAP_DATA_DIR=/data \
+    -v "$IMAP_DATA_DIR:/data" \
+    imap-l3 "$@"
+echo "Done!"
